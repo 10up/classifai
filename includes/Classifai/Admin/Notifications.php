96eedcb1f697815f1f2b201dce0888b847370081
Ryan Welcher <ryan.welcher@10up.com>
2019-02-15 12:21:49 -0500

    Adds Notifications class and registers it

---
+++ includes/Classifai/Admin/Notifications.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Classifai\Admin;
+
+
+class Notifications {
+
+	/**
+	 * Check to see if we can register this class.
+	 *
+	 * @todo look for the option.
+	 *
+	 * @return bool
+	 */
+	public function can_register() {
+		return true;
+	}
+
+	public function register() {
+
+	}
+}
