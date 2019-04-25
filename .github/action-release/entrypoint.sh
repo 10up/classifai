3fdf64c60cff3b05e01ba50c7c13cd4d65908eed
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-04-25 15:19:14 -0600

    Use rsync to copy files because `git archive` respects `.gitignore`
    This has a problem because it's deleting the `.git` directory which wipes out the worktree info :( May need to use a different dir or find some other rsync magic

--- .github/action-release/entrypoint.sh
+++ .github/action-release/entrypoint.sh
@@ -13,15 +13,8 @@ fi
 # I think we are already here but just in case
 cd "$GITHUB_WORKSPACE"
 
-# "Export" a cleaned copy to a temp directory
-TMP_DIR="/github/archivetmp"
-mkdir "$TMP_DIR"
-
 git config --global user.email "10upbot+github@10up.com"
 git config --global user.name "10upbot on GitHub"
-
-# This will exclude everything in the .gitattributes file with the export-ignore flag
-git archive HEAD | tar x --directory="$TMP_DIR"
 
 # Use worktree to check out into a subfolder so we don't have lingering files hanging out
 # This assumes there's already a stable branch!
@@ -29,7 +22,7 @@ git worktree add -B stable release origin/stable
 
 # Copy from clean copy into release folder
 # The --delete flag will delete anything in destination that no longer exists in source
-rsync -r "$TMP_DIR/" release/ --delete
+rsync -r "$GITHUB_WORKSPACE/" release/ --exclude-from=".github/action-release/rsync-filter.txt" --delete
 
 # Commit everything
 cd release
