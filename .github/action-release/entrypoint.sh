58d54f40d4516000d044290d57d6352e7afc272b
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-04-25 13:50:42 -0600

    Use worktree instead of direct branch switch

--- .github/action-release/entrypoint.sh
+++ .github/action-release/entrypoint.sh
@@ -23,14 +23,16 @@ git config --global user.name "10upbot on GitHub"
 # This will exclude everything in the .gitattributes file with the export-ignore flag
 git archive HEAD | tar x --directory="$TMP_DIR"
 
+# Use worktree to check out into a subfolder so we don't have lingering files hanging out
 # This assumes there's already a stable branch!
-git checkout stable
+git worktree add -B stable release origin/stable
 
-# Copy from clean copy into workspace
+# Copy from clean copy into release folder
 # The --delete flag will delete anything in destination that no longer exists in source
-rsync -r "$TMP_DIR/" / --delete
+rsync -r "$TMP_DIR/" release/ --delete
 
 # Commit everything
+cd release
 git commit -am "Committing latest production-ready version of master"
 git push origin stable
 
