f015073d1a5ec87709b16483ecc9712cbdea8f38
Ryan Welcher <ryan.welcher@10up.com>
2019-08-19 14:51:56 -0400

    Adds Image Tag taxonomy to the Image Processing service

---
+++ includes/Classifai/Taxonomy/ImageTagTaxonomy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Classifai\Taxonomy;
+
+class ImageTagTaxonomy extends AbstractTaxonomy {
+
+	/**
+	 * Get the ClassifAI category taxonomy name.
+	 */
+	public function get_name() {
+		return 'classifai-image-tags';
+	}
+
+	/**
+	 * Get the ClassifAI category taxonomy label.
+	 */
+	public function get_singular_label() {
+		return esc_html__( 'Image Tag', 'classifai' );
+	}
+
+	/**
+	 * Get the ClassifAI category taxonomy plural label.
+	 */
+	public function get_plural_label() {
+		return esc_html__( 'Image Tags', 'classifai' );
+	}
+
+	/**
+	 * Get the ClassifAI category taxonomy visibility.
+	 */
+	public function get_visibility() {
+		return true;
+	}
+
+}
