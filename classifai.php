fc35f510afde903b5f5bd96474ca1c6a514bcf50
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-09-26 15:06:55 -0600

    Version bump for 1.4.0

--- classifai.php
+++ classifai.php
@@ -3,7 +3,7 @@
  * Plugin Name:     ClassifAI
  * Plugin URI:      https://github.com/10up/classifai
  * Description:     Enhance your WordPress content with Artificial Intelligence and Machine Learning services.
- * Version:         1.3.2
+ * Version:         1.4.0
  * Author:          10up
  * Author URI:      https://10up.com
  * License:         GPLv2
