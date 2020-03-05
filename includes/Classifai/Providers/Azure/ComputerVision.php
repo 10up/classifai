ce02ca478c2e5658e2438fd9fbba8fa22b0ed7c1
Helen Hou-Sandi <helen.y.hou@gmail.com>
2020-03-04 21:45:55 -0600

    Complete filter docs

--- includes/Classifai/Providers/Azure/ComputerVision.php
+++ includes/Classifai/Providers/Azure/ComputerVision.php
@@ -150,10 +150,13 @@ class ComputerVision extends Provider {
 		 * Filters whether to apply smart cropping to the current image.
 		 *
 		 * @since 1.5.0
+		 * @hook classifai_should_smart_crop_image
 		 *
-		 * @param boolean Whether to apply smart cropping. The default value is set in ComputerVision settings.
-		 * @param array   Image metadata.
-		 * @param int     The attachment ID.
+		 * @param {bool}  $should_smart_crop Whether to apply smart cropping. The default value is set in ComputerVision settings.
+		 * @param {array} $metadata          Image metadata.
+		 * @param {int}   $attachment_id     The attachment ID.
+		 *
+		 * @return {bool} Whether to apply smart cropping.
 		 */
 		if ( ! apply_filters( 'classifai_should_smart_crop_image', $should_smart_crop, $metadata, $attachment_id ) ) {
 			return $metadata;
@@ -282,9 +285,12 @@ class ComputerVision extends Provider {
 		/**
 		 * Filter the captions returned from the API.
 		 *
-		 * @param array $captions. The caption data.
+		 * @since 1.4.0
+		 * @hook classifai_computer_vision_captions
 		 *
-		 * @return array $captions The filtered caption data.
+		 * @param {array} $captions The returned caption data.
+		 *
+		 * @return {array} The filtered caption data.
 		 */
 		$captions = apply_filters( 'classifai_computer_vision_captions', $captions );
 		// If $captions isn't an array, don't save them.
@@ -325,9 +331,12 @@ class ComputerVision extends Provider {
 		/**
 		 * Filter the tags returned from the API.
 		 *
-		 * @param array $tags. The image tag data.
+		 * @since 1.4.0
+		 * @hook classifai_computer_vision_image_tags
 		 *
-		 * @return array $tags The filtered image tags.
+		 * @param {array} $tags The image tag data.
+		 *
+		 * @return {array} The filtered image tags.
 		 */
 		$tags = apply_filters( 'classifai_computer_vision_image_tags', $tags );
 		// If $tags isn't an array, don't save them.
