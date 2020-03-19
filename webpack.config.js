5160a75370382fcaf5d03a4d221970d7e90745d8
Ryan Welcher <ryan.welcher@10up.com>
2020-03-19 10:56:00 -0400

    Update webpack config for a better build experience. -- Adds DependencyExtractionPlugin -- removes .min from compiles files -- Adds webpackbar and plugin to clean the dist folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
+const WebpackBar = require( 'webpackbar' );
+const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
 
 module.exports = {
 	entry: {
@@ -7,7 +10,7 @@ module.exports = {
 		'admin': './src/js/admin.js'
 	},
 	output: {
-		filename: '[name].min.js',
+		filename: '[name].js',
 		path: path.resolve( './dist/js')
 	},
 	module: {
@@ -28,5 +31,15 @@ module.exports = {
 				}
 			}
 		],
-	}
+	},
+	plugins: [
+		// Clean the `dist` folder on build.
+		new CleanWebpackPlugin(),
+
+		// Fancy WebpackBar.
+		new WebpackBar(),
+
+		// Extract dependencies.
+		new DependencyExtractionWebpackPlugin( { injectPolyfill: true } ),
+	]
 };
