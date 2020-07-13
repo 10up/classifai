422bdd98e177dc68391d8ebf93769c5c2c54d674
Tung Du <dinhtungdu@gmail.com>
2020-07-13 13:08:06 +0700

    test: add settings tests

--- tests/e2e/wpacceptance/Settings.php
+++ tests/e2e/wpacceptance/Settings.php
@@ -74,5 +74,50 @@ class Settings extends \TestCaseBase {
 
 		$I->seeLink( 'Image Processing' );
 	}
+
+	/**
+	 * @testdox When the user click on ClassifAI top menu, it shows registration form.
+	 */
+	public function testRegistrationFormShows() {
+		$I = $this->openBrowserPage();
+
+		$I->login();
+
+		$I->moveTo( 'wp-admin/admin.php?page=classifai_settings' );
+
+		$I->seeText( 'Registered Email ');
+
+		$I->seeText( 'Registration Key');
+	}
+
+	/**
+	 * @testdox When the user goes to the Language Processing submenu, it loads the Watson settings page.
+	 */
+	public function testWatsonSettingsShows() {
+		$I = $this->openBrowserPage();
+
+		$I->login();
+
+		$I->moveTo( 'wp-admin/admin.php?page=language_processing' );
+
+		$I->seeText( 'Natural Language Understanding' );
+
+		$I->seeText( 'Entity' );
+	}
+
+	/**
+	 * @testdox When the user goes to the Image Processing submenu, it loads the Computer Vision settings page.
+	 */
+	public function testComputerVisionSettingsShows() {
+		$I = $this->openBrowserPage();
+
+		$I->login();
+
+		$I->moveTo( 'wp-admin/admin.php?page=image_processing' );
+
+		$I->seeText( 'Computer Vision' );
+
+		$I->seeText( 'Automatically Caption Images' );
+	}
 }
 
