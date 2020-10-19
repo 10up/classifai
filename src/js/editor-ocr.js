81c6dd24a281b3b64d934f970a05aa72f12e23b1
Tung Du <dinhtungdu@gmail.com>
2020-10-19 22:42:19 +0700

    feat: auto insert description generated by ocr after image block

--- src/js/editor-ocr.js
+++ src/js/editor-ocr.js
@@ -1,56 +1,49 @@
-const { subscribe, useSelect, select, dispatch } = wp.data;
+/* global lodash */
+const { subscribe, select, dispatch } = wp.data;
 const { createBlock } = wp.blocks;
-const apiFetch = wp.apiFetch;
-const { countBy } = lodash;
+const { apiFetch } = wp;
+const { find, debounce } = lodash;
 
-const { getSelectedBlock, getBlock, getNextBlockClientId, getBlockIndex } = select( 'core/block-editor');
+const { getSelectedBlock, getBlocks, getBlockIndex } = select( 'core/block-editor' );
 
-subscribe( async () => {
-	// when a new block gets inserted it automatically gets selected
-	// also when an image gets selected the image block is the currently
-	// selected block in the editor
+let currentBlocks;
+
+subscribe( debounce( async () => {
+	const newBlocks = getBlocks();
+	const prevBlocks = currentBlocks;
+	currentBlocks = newBlocks;
+
 	const currentBlock = getSelectedBlock();
 
-	// we only whant to do anything if the block is an image block
 	if ( ! currentBlock || 'core/image' !== currentBlock.name ) {
-		return
-	}
-
-	// we can get access to all the attributes of the image block
-	const imageId = currentBlock.attributes.id;
-
-	if ( ! imageId ) {
 		return;
 	}
 
-	const nextBlock = getBlock( getNextBlockClientId() );
+	if ( ! currentBlock.attributes.id ) {
+		return;
+	}
 
-	if (
-		nextBlock.name === "core/paragraph"
-		&& nextBlock.attributes.hasOwnProperty( 'className' )
-		&& nextBlock.attributes.className.includes( 'classifai-ocr-text' )
-	) {
+	const prevBlock = find( prevBlocks, block => block.clientId === currentBlock.clientId );
+
+	if ( ! prevBlock || prevBlock.attributes.id === currentBlock.attributes.id ) {
 		return;
 	}
 
 	const imageBlockIndex = getBlockIndex( currentBlock.clientId );
 
-	const media = await apiFetch( { path: '/wp/v2/media/' + imageId } );
+	const media = await apiFetch( { path: `/wp/v2/media/${currentBlock.attributes.id}` } );
 
 	if (
-		! media.hasOwnProperty('caption')
-		|| ! media.caption.hasOwnProperty('rendered')
-		|| ! media.caption.rendered
+		! Object.prototype.hasOwnProperty.call( media, 'description' )
+		|| ! Object.prototype.hasOwnProperty.call( media.description, 'rendered' )
+		|| ! media.description.rendered
 	) {
 		return;
 	}
 
 	const newBlock = createBlock( 'core/paragraph', {
-		content: media.caption.rendered,
-		className: 'classifai-ocr-text'
+		content: media.description.rendered.replace( /(<([^>]+)>)/gi, '' )
 	} );
-	dispatch( 'core/block-editor' ).insertBlock(
-		newBlock,
-		imageBlockIndex + 1
-	);
-});
+
+	dispatch( 'core/block-editor' ).insertBlock( newBlock, imageBlockIndex + 1 );
+} ), 100 );
