5772efce5026ec38099d16a41756693a4a251fe5
Tung Du <dinhtungdu@gmail.com>
2021-06-07 02:22:45 +0000

    fix the autoload

--- autoload.php
+++ autoload.php
@@ -68,8 +68,7 @@ class Psr4AutoloaderClass {
 
 		// work backwards through the namespace names of the fully-qualified
 		// class name to find a mapped file name
-		$pos = strrpos( $prefix, '\\' );
-		while ( false !== $pos ) {
+		while ( false !== $pos = strrpos( $prefix, '\\' ) ) { // phpcs:ignore WordPress.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition
 
 			// retain the trailing namespace separator in the prefix
 			$prefix = substr( $class, 0, $pos + 1 );
