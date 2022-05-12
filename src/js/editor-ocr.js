00476971b33b2d1a0523b35a030921fc278aa61f
Dharmesh Patel <dspatel44@gmail.com>
2022-05-12 11:22:17 +0530

    format js for eslint fix using `npm run format-js`

--- src/js/editor-ocr.js
+++ src/js/editor-ocr.js
@@ -13,34 +13,34 @@ const { useState, Fragment } = wp.element; // eslint-disable-line no-unused-vars
 /**
  * Icon for insert button.
  */
-const insertIcon = <span className="dashicons dashicons-editor-paste-text"></span>;
+const insertIcon = <span className="dashicons dashicons-editor-paste-text" />;
 
 /**
  * Get image scanned text using media api.
  *
  * @param {int} imageId - Image ID.
-*/
-const getImageOcrScannedText = async ( imageId ) => {
-	const media = await apiFetch( { path: `/wp/v2/media/${imageId}` } );
+ */
+const getImageOcrScannedText = async (imageId) => {
+	const media = await apiFetch({ path: `/wp/v2/media/${imageId}` });
 
 	if (
-		! Object.prototype.hasOwnProperty.call( media, 'classifai_has_ocr' )
-		|| ! media.classifai_has_ocr
+		!Object.prototype.hasOwnProperty.call(media, 'classifai_has_ocr') ||
+		!media.classifai_has_ocr
 	) {
 		return false;
 	}
 
 	if (
-		! Object.prototype.hasOwnProperty.call( media, 'description' )
-		|| ! Object.prototype.hasOwnProperty.call( media.description, 'rendered' )
-		|| ! media.description.rendered
+		!Object.prototype.hasOwnProperty.call(media, 'description') ||
+		!Object.prototype.hasOwnProperty.call(media.description, 'rendered') ||
+		!media.description.rendered
 	) {
 		return false;
 	}
 
 	return media.description.rendered
-		.replace( /(<([^>]+)>)/gi, '' )
-		.replace( /(\r\n|\n|\r)/gm,'' )
+		.replace(/(<([^>]+)>)/gi, '')
+		.replace(/(\r\n|\n|\r)/gm, '')
 		.trim();
 };
 
@@ -50,129 +50,145 @@ const getImageOcrScannedText = async ( imageId ) => {
  * @param {int} clientId - Client ID of image block.
  * @param {int} imageId - Image ID.
  * @param {string} scannedText - Text to insert to editor.
-*/
-const insertOcrScannedText = async ( clientId, imageId, scannedText ) => {
-	const { getBlockIndex } = select( 'core/block-editor' );
+ */
+const insertOcrScannedText = async (clientId, imageId, scannedText) => {
+	const { getBlockIndex } = select('core/block-editor');
 
-	if( ! scannedText ) {
+	if (!scannedText) {
 		return;
 	}
 
-	const groupBlock = createBlock( 'core/group', {
+	const groupBlock = createBlock('core/group', {
 		anchor: `classifai-ocr-${imageId}`,
 		className: 'is-style-classifai-ocr-text',
-	} );
+	});
 
-	const textBlock = createBlock( 'core/paragraph', {
+	const textBlock = createBlock('core/paragraph', {
 		content: scannedText,
-	} );
+	});
 
-	await dispatch( 'core/block-editor' ).insertBlock( groupBlock, getBlockIndex( clientId ) + 1 );
-	dispatch( 'core/block-editor' ).insertBlock( textBlock, 0, groupBlock.clientId );
+	await dispatch('core/block-editor').insertBlock(groupBlock, getBlockIndex(clientId) + 1);
+	dispatch('core/block-editor').insertBlock(textBlock, 0, groupBlock.clientId);
 };
 
 /**
  * Check if current post has OCR block.
  *
  * @param {int} imageId - Image ID.
- * @param {array} blocks - Current blocks of current post.
+ * @param {Array} blocks - Current blocks of current post.
  */
-const hasOcrBlock = ( imageId, blocks = [] ) => {
-	if ( 0 === blocks.length ) {
-		const { getBlocks } = select( 'core/block-editor' );
+const hasOcrBlock = (imageId, blocks = []) => {
+	if (blocks.length === 0) {
+		const { getBlocks } = select('core/block-editor');
 		blocks = getBlocks();
 	}
-	return !! find( blocks, block => block.attributes.anchor === `classifai-ocr-${imageId}` );
+	return !!find(blocks, (block) => block.attributes.anchor === `classifai-ocr-${imageId}`);
 };
 
 /**
  * Add insert button to toolbar.
-*/
-const imageOcrControl = createHigherOrderComponent( ( BlockEdit ) => { // eslint-disable-line no-unused-vars
-	return ( props ) => {
-		const [ isModalOpen, setModalOpen ] = useState( false );
+ */
+const imageOcrControl = createHigherOrderComponent((BlockEdit) => {
+	// eslint-disable-line no-unused-vars
+	return (props) => {
+		const [isModalOpen, setModalOpen] = useState(false);
 		const { attributes, clientId, isSelected, name, setAttributes } = props;
 
-		if ( ! isSelected || 'core/image' != name ) {
+		if (!isSelected || name != 'core/image') {
 			return <BlockEdit {...props} />;
 		}
 
-		if ( ! attributes.ocrChecked && attributes.id ) {
-			getImageOcrScannedText( attributes.id )
-				.then( data => {
-					if ( data ) {
-						setAttributes( {
-							ocrScannedText: data,
-							ocrChecked: true,
-						} );
-						setModalOpen( true );
-					} else {
-						setAttributes( {
-							ocrChecked: true,
-						} );
-					}
-				} );
+		if (!attributes.ocrChecked && attributes.id) {
+			getImageOcrScannedText(attributes.id).then((data) => {
+				if (data) {
+					setAttributes({
+						ocrScannedText: data,
+						ocrChecked: true,
+					});
+					setModalOpen(true);
+				} else {
+					setAttributes({
+						ocrChecked: true,
+					});
+				}
+			});
 		}
-
 
 		return (
 			<Fragment>
 				<BlockEdit {...props} />
-				{attributes.ocrScannedText && <BlockControls>
-					<ToolbarGroup>
-						<ToolbarButton
-							label={__( 'Insert scanned text into content', 'classifai' )}
-							icon={insertIcon}
-							onClick={() => insertOcrScannedText( clientId, attributes.id, attributes.ocrScannedText )}
-							disabled={hasOcrBlock( attributes.id )}
-						/>
-					</ToolbarGroup>
-				</BlockControls>}
-				{isModalOpen && <Modal title={__( 'ClassifAI detected text in your image', 'classifai' )}>
-					<p>{__( 'Would you like you insert the scanned text under this image block? This enhances search indexing and accessibility for readers.', 'classifai' )}</p>
-					<Flex align='flex-end' justify='flex-end'>
-						<FlexItem>
-							<Button isPrimary onClick={() => {
-								insertOcrScannedText( clientId, attributes.id, attributes.ocrScannedText );
-								setModalOpen( false );
-							}}>
-								{__( 'Insert text', 'classifai' )}
-							</Button>
-						</FlexItem>
-						<FlexItem>
-							<Button isSecondary onClick={ () => setModalOpen( false ) }>
-								{__( 'Dismiss', 'classifai' )}
-							</Button>
-						</FlexItem>
-					</Flex>
-				</Modal> }
+				{attributes.ocrScannedText && (
+					<BlockControls>
+						<ToolbarGroup>
+							<ToolbarButton
+								label={__('Insert scanned text into content', 'classifai')}
+								icon={insertIcon}
+								onClick={() =>
+									insertOcrScannedText(
+										clientId,
+										attributes.id,
+										attributes.ocrScannedText,
+									)
+								}
+								disabled={hasOcrBlock(attributes.id)}
+							/>
+						</ToolbarGroup>
+					</BlockControls>
+				)}
+				{isModalOpen && (
+					<Modal title={__('ClassifAI detected text in your image', 'classifai')}>
+						<p>
+							{__(
+								'Would you like you insert the scanned text under this image block? This enhances search indexing and accessibility for readers.',
+								'classifai',
+							)}
+						</p>
+						<Flex align="flex-end" justify="flex-end">
+							<FlexItem>
+								<Button
+									isPrimary
+									onClick={() => {
+										insertOcrScannedText(
+											clientId,
+											attributes.id,
+											attributes.ocrScannedText,
+										);
+										setModalOpen(false);
+									}}
+								>
+									{__('Insert text', 'classifai')}
+								</Button>
+							</FlexItem>
+							<FlexItem>
+								<Button isSecondary onClick={() => setModalOpen(false)}>
+									{__('Dismiss', 'classifai')}
+								</Button>
+							</FlexItem>
+						</Flex>
+					</Modal>
+				)}
 			</Fragment>
 		);
 	};
-}, 'imageOcrControl' );
+}, 'imageOcrControl');
 
-addFilter(
-	'editor.BlockEdit',
-	'classifai/image-processing-ocr',
-	imageOcrControl
-);
+addFilter('editor.BlockEdit', 'classifai/image-processing-ocr', imageOcrControl);
 
 /**
  * Add custom attribute for OCR to image block.
  *
  * @param {object} settings - Block settings.
  * @param {string} name - Block name.
-*/
-const modifyImageAttributes = ( settings, name ) => {
-	if ( 'core/image' !== name ) {
+ */
+const modifyImageAttributes = (settings, name) => {
+	if (name !== 'core/image') {
 		return settings;
 	}
 
-	if ( settings.attributes ) {
-
+	if (settings.attributes) {
 		settings.attributes.ocrScannedText = {
 			type: 'string',
-			default: ''
+			default: '',
 		};
 		settings.attributes.ocrChecked = {
 			type: 'boolean',
@@ -182,16 +198,12 @@ const modifyImageAttributes = ( settings, name ) => {
 	return settings;
 };
 
-addFilter(
-	'blocks.registerBlockType',
-	'classifai/image-processing-ocr',
-	modifyImageAttributes
-);
+addFilter('blocks.registerBlockType', 'classifai/image-processing-ocr', modifyImageAttributes);
 
-wp.blocks.registerBlockStyle( 'core/group', {
+wp.blocks.registerBlockStyle('core/group', {
 	name: 'classifai-ocr-text',
-	label: __( 'Scanned Text from Image', 'classifai' ),
-} );
+	label: __('Scanned Text from Image', 'classifai'),
+});
 
 {
 	/**
@@ -200,73 +212,83 @@ wp.blocks.registerBlockStyle( 'core/group', {
 	let previousSelectedBlock;
 	let activeBlocks = [];
 
-	subscribe( debounce( () => {
-		const blockEditor = select( 'core/block-editor' );
-		const selectedBlock = blockEditor.getSelectedBlock();
-		const blocks = blockEditor.getBlocks();
+	subscribe(
+		debounce(() => {
+			const blockEditor = select('core/block-editor');
+			const selectedBlock = blockEditor.getSelectedBlock();
+			const blocks = blockEditor.getBlocks();
 
-		// If no selected block, return early and if needed, remove classes
-		if ( null === selectedBlock ) {
+			// If no selected block, return early and if needed, remove classes
+			if (selectedBlock === null) {
+				maybeClearCss();
+				previousSelectedBlock = selectedBlock;
+
+				return;
+			}
+
+			// If the current selected block is the same as previously or the current block is styled, return early
+			if (
+				selectedBlock === previousSelectedBlock ||
+				activeBlocks.includes(selectedBlock.clientId)
+			) {
+				return;
+			}
 
 			maybeClearCss();
+
 			previousSelectedBlock = selectedBlock;
 
-			return;
-		}
+			if (selectedBlock.name === 'core/image') {
+				const ocrBlock = find(
+					blocks,
+					(block) =>
+						block.attributes.anchor === `classifai-ocr-${selectedBlock.attributes.id}`,
+				);
 
-		// If the current selected block is the same as previously or the current block is styled, return early
-		if ( selectedBlock === previousSelectedBlock || activeBlocks.includes( selectedBlock.clientId ) ) {
-			return;
-		}
+				if (undefined !== ocrBlock) {
+					generateCss([ocrBlock.clientId, selectedBlock.clientId]);
+				}
+			} else {
+				const rootBlock = blockEditor.getBlock(
+					blockEditor.getBlockHierarchyRootClientId(selectedBlock.clientId),
+				);
 
-		maybeClearCss();
+				if (rootBlock.name === 'core/group') {
+					let imageId = /classifai-ocr-([0-9]+)/.exec(rootBlock.attributes.anchor);
 
-		previousSelectedBlock = selectedBlock;
+					if (imageId !== null) {
+						[, imageId] = imageId;
 
-		if ( 'core/image' === selectedBlock.name ) {
-			const ocrBlock = find( blocks, block => block.attributes.anchor === `classifai-ocr-${selectedBlock.attributes.id}` );
+						const imageBlock = find(blocks, (block) => block.attributes.id == imageId);
 
-			if ( undefined !== ocrBlock ) {
-				generateCss( [ocrBlock.clientId, selectedBlock.clientId] );
-			}
-		} else {
-			const rootBlock = blockEditor.getBlock( blockEditor.getBlockHierarchyRootClientId( selectedBlock.clientId ) );
-
-			if ( 'core/group' === rootBlock.name ) {
-				let imageId = /classifai-ocr-([0-9]+)/.exec( rootBlock.attributes.anchor );
-
-				if ( null !== imageId ) {
-					[ , imageId ] = imageId;
-
-					const imageBlock = find( blocks, block => block.attributes.id == imageId );
-
-					if ( undefined !== imageBlock ) {
-						generateCss( [imageBlock.clientId, rootBlock.clientId] );
+						if (undefined !== imageBlock) {
+							generateCss([imageBlock.clientId, rootBlock.clientId]);
+						}
 					}
 				}
 			}
-		}
-	}, 100 ) );
+		}, 100),
+	);
 
 	/**
 	 * Create internal style tag if needed.
 	 */
 	const createStyle = () => {
-		const head = document.head || document.getElementsByTagName( 'head' )[0];
-		const style = document.createElement( 'style' );
-		style.setAttribute( 'id', 'classifai-ocr-style' );
-		head.appendChild( style );
+		const head = document.head || document.getElementsByTagName('head')[0];
+		const style = document.createElement('style');
+		style.setAttribute('id', 'classifai-ocr-style');
+		head.appendChild(style);
 		return style;
 	};
 
 	/**
 	 * Generate CSS for current focused image/ocr block.
 	 *
-	 * @param {array} ids Array of id.
+	 * @param {Array} ids Array of id.
 	 */
-	const generateCss = ( ids ) => {
-		const style = document.getElementById( 'classifai-ocr-style' ) ?? createStyle();
-		const selectors = ids.map( id => `#block-${id}:before` ).join( ', ' );
+	const generateCss = (ids) => {
+		const style = document.getElementById('classifai-ocr-style') ?? createStyle();
+		const selectors = ids.map((id) => `#block-${id}:before`).join(', ');
 		const css = `${selectors} {
 			content: "";
 			position: absolute;
@@ -279,7 +301,7 @@ wp.blocks.registerBlockStyle( 'core/group', {
 			opacity: 0.25;
 		}`;
 
-		style.appendChild( document.createTextNode( css ) );
+		style.appendChild(document.createTextNode(css));
 		activeBlocks = ids;
 	};
 
@@ -287,13 +309,13 @@ wp.blocks.registerBlockStyle( 'core/group', {
 	 * Clear generated CSS.
 	 */
 	const maybeClearCss = () => {
-		if ( 0 === activeBlocks.length ) {
+		if (activeBlocks.length === 0) {
 			return;
 		}
 
-		const style = document.getElementById( 'classifai-ocr-style' );
+		const style = document.getElementById('classifai-ocr-style');
 
-		if ( style ) {
+		if (style) {
 			style.innerText = '';
 		}
 
--- src/js/editor.js
+++ src/js/editor.js
@@ -1,23 +1,27 @@
-
 const { subscribe, select, dispatch } = wp.data;
 
 let saveHappened = false;
 let showingNotice = false;
 
-subscribe( () => {
-
-	if ( false === saveHappened ) {
-		saveHappened = true === wp.data.select( 'core/editor' ).isSavingPost();
+subscribe(() => {
+	if (saveHappened === false) {
+		saveHappened = wp.data.select('core/editor').isSavingPost() === true;
 	}
 
-	if ( saveHappened && false === wp.data.select( 'core/editor' ).isSavingPost() && false === showingNotice ) {
-		const meta = select( 'core/editor' ).getCurrentPostAttribute( 'meta' );
-		if ( meta && meta._classifai_error ) {
+	if (
+		saveHappened &&
+		wp.data.select('core/editor').isSavingPost() === false &&
+		showingNotice === false
+	) {
+		const meta = select('core/editor').getCurrentPostAttribute('meta');
+		if (meta && meta._classifai_error) {
 			showingNotice = true;
-			const error = JSON.parse( meta._classifai_error );
-			dispatch( 'core/notices' ).createErrorNotice( `Failed to classify content with the IBM Watson NLU API. Error: ${ error.code } - ${ error.message }` );
+			const error = JSON.parse(meta._classifai_error);
+			dispatch('core/notices').createErrorNotice(
+				`Failed to classify content with the IBM Watson NLU API. Error: ${error.code} - ${error.message}`,
+			);
 			saveHappened = false;
 			showingNotice = false;
 		}
 	}
-} );
+});
