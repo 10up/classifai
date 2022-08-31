24001e754b47b098dec03c875c6342b7aff9aa4c
Jeffrey Paul <jeffpaul@hotmail.com>
2022-08-30 21:23:56 -0500

    add missing punctuation

--- classifai.php
+++ classifai.php
@@ -26,7 +26,7 @@ register_activation_hook(
 				sprintf(
 					wp_kses(
 						/* translators: PHP Update guide URL */
-						__( 'ClassifAI requires PHP version 7.2 <a href="%s">Click here</a> to learn how to update your PHP version.', 'classifai' ),
+						__( 'ClassifAI requires PHP version 7.2. <a href="%s">Click here</a> to learn how to update your PHP version.', 'classifai' ),
 						array(
 							'a' => array( 'href' => array() ),
 						)
