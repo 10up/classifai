7500b1c9495e5ba370a88e7cf8ce6a989d8e78ff
Dharmesh Patel <dspatel44@gmail.com>
2023-03-30 18:59:50 +0530

    Added header control links.

--- includes/Classifai/Admin/templates/classifai-header.php
+++ includes/Classifai/Admin/templates/classifai-header.php
@@ -14,7 +14,43 @@
 			</div>
 		</div>
 		<div id="classifai-header-controls">
-
+			<?php
+			$page = isset( $_GET['page'] ) ? sanitize_text_field( wp_unslash( $_GET['page'] ) ) : ''; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			if ( 'classifai_setup' === $page ) {
+				?>
+				<div class="header-control-item">
+					<a href="<?php echo esc_url( admin_url( 'admin.php?page=classifai_settings' ) ); ?>" class="classifai-help-links">
+						<span class="dashicons dashicons-admin-generic"></span>
+						<span class="control-item-text"><?php esc_html_e( 'Settings', 'classifai' ); ?></span>
+					</a>
+				</div>
+				<?php
+			} else {
+				$onboarding_options = get_option( 'classifai_onboarding_options', array() );
+				if ( ! isset( $onboarding_options['status'] ) || 'completed' !== $onboarding_options['status'] ) {
+					?>
+					<div class="header-control-item">
+						<a href="<?php echo esc_url( admin_url( 'admin.php?page=classifai_setup' ) ); ?>" class="classifai-help-links">
+							<span class="dashicons dashicons-admin-tools"></span>
+							<span class="control-item-text"><?php esc_html_e( 'Set up', 'classifai' ); ?></span>
+						</a>
+					</div>
+					<?php
+				}
+			}
+			?>
+			<div class="header-control-item">
+				<a href="#" class="classifai-help-links classifai-help">
+					<span class="dashicons dashicons-editor-help"></span>
+					<span class="control-item-text"><?php esc_html_e( 'Help', 'classifai' ); ?></span>
+				</a>
+				<template id="help-menu-template">
+					<div class="classifai-help-menu">
+						<a class="classifai-help-menu__menu-item" target="_blank" href="https://github.com/10up/classifai#frequently-asked-questions"><?php esc_html_e( 'FAQs', 'classifai' ); ?></a>
+						<a class="classifai-help-menu__menu-item" target="_blank"  href="https://github.com/10up/classifai/issues/new/choose"><?php esc_html_e( 'Report issue/enhancement', 'classifai' ); ?></a>
+					</div>
+				</template>
+			</div>
 		</div>
 	</div>
 </header>
--- src/js/admin.js
+++ src/js/admin.js
@@ -1,5 +1,29 @@
 /* global ClassifAI */
 import '../scss/admin.scss';
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
+
+document.addEventListener( 'DOMContentLoaded', function() {
+	const template = document.getElementById( 'help-menu-template' );
+	const container = document.createElement( 'div' );
+	container.appendChild( document.importNode( template.content, true ) );
+
+	tippy( '.classifai-help', {
+		allowHTML: true,
+		content: container.innerHTML,
+		trigger: 'click', // mouseenter, click, focus; manual.
+		placement: 'bottom-end',
+		arrow: true,
+		arrowType: 'round',
+		animation: 'scale',
+		duration: [ 250, 200 ],
+		theme: 'light',
+		distance: 12,
+		interactive: true,
+		showOnInit: true,
+	} );
+} );
 
 ( () => {
 	const $toggler = document.getElementById( 'classifai-waston-cred-toggle' );
