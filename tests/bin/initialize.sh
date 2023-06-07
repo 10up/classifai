ea342da90b9fc81ef47c701104efebbbfbd0b3d0
Darin Kotter <darin.kotter@gmail.com>
2023-06-07 10:44:34 -0600

    Update e2e dependencies and fix a few issues

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npm run env run tests-wordpress "chmod -c ugo+w /var/www/html"
-npm run env run tests-cli "wp rewrite structure '/%postname%/' --hard"
+npm run env run tests-wordpress chmod -- -c ugo+w /var/www/html
+npm run env run tests-cli wp rewrite structure '/%postname%/' -- --hard
