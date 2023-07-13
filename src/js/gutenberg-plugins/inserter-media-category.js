d6d05ed7c5b683c933c3b6ea808bf6278a7e9cd2
Darin Kotter <darin.kotter@gmail.com>
2023-07-13 11:13:44 -0600

    Remove code that is no longer needed. Refactor our JS a bit to only load once

--- src/js/gutenberg-plugins/inserter-media-category.js
+++ src/js/gutenberg-plugins/inserter-media-category.js
@@ -1,78 +1,30 @@
-import { dispatch } from '@wordpress/data';
-import { Component } from '@wordpress/element';
+import { dispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
 
 const { classifaiDalleData, wpApiSettings } = window;
 
-class RegisterInserterMediaCategory extends Component {
-	constructor( props ) {
-		super( props );
-		this.state = {
-			loaded: false,
-		};
-	}
+const RegisterInserterMediaCategory = () => {
+	const { inserterMediaCategories } = useSelect(
+		( select ) => select( 'core/block-editor' ).getSettings(),
+		[]
+	);
 
-	register() {
-		if (
-			typeof dispatch( 'core/block-editor' )
-				.registerInserterMediaCategory !== 'function'
-		) {
-			return;
-		}
-
-		return dispatch( 'core/block-editor' ).registerInserterMediaCategory( {
-			name: 'classifai-generate-image',
-			labels: {
-				name: classifaiDalleData.tabText,
-				search_items: __( 'Enter a prompt', 'classifai' ),
-			},
-			mediaType: 'image',
-			async fetch( { search = '' } ) {
-				if ( ! search ) {
-					return [];
-				}
-
-				const url = new URL(
-					wpApiSettings.root + classifaiDalleData.endpoint
-				);
-				url.searchParams.set( 'prompt', search );
-				url.searchParams.set( 'format', 'b64_json' );
-
-				const response = await window.fetch( url, {
-					headers: {
-						'X-WP-Nonce': wpApiSettings.nonce,
-					},
-				} );
-
-				if ( response.ok ) {
-					const jsonResponse = await response.json();
-					return jsonResponse.map( ( item ) => ( {
-						title: search,
-						url: `data:image/png;base64,${ item.url }`,
-						previewUrl: `data:image/png;base64,${ item.url }`,
-						id: undefined,
-						alt: search,
-						caption: classifaiDalleData.caption,
-					} ) );
-				}
-
-				return [];
-			},
-			isExternalResource: true,
-		} );
-	}
-
-	render() {
-		if ( ! this.state.loaded ) {
-			this.register();
-		}
-
+	// If we have no categories yet, assume things aren't ready yet.
+	if ( ! inserterMediaCategories ) {
 		return null;
 	}
-}
 
-const registerInserterMediaCategory = () => {
+	// If our custom category is already registered, don't register it again.
+	if (
+		inserterMediaCategories.some(
+			( { name } ) => name === 'classifai-generate-image'
+		)
+	) {
+		return null;
+	}
+
+	// Ensure the function we want to use is available.
 	if (
 		typeof dispatch( 'core/block-editor' ).registerInserterMediaCategory !==
 		'function'
@@ -125,5 +77,5 @@ const registerInserterMediaCategory = () => {
 };
 
 registerPlugin( 'classifai-inserter-media-category', {
-	render: registerInserterMediaCategory,
+	render: RegisterInserterMediaCategory,
 } );
