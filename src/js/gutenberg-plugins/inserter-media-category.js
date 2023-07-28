ff2990853941b1ef74f4ea980837a4b7e8614aae
Darin Kotter <darin.kotter@gmail.com>
2023-07-28 14:46:20 -0600

    Refactor how we register the media category by copying what Jetpack did. This is a cleaner approach than registering this within a plugin

--- src/js/gutenberg-plugins/inserter-media-category.js
+++ src/js/gutenberg-plugins/inserter-media-category.js
@@ -1,78 +1,67 @@
+/**
+ * Some code here was copied from Jetpack's implementation of the inserter media category.
+ * See https://github.com/Automattic/jetpack/pull/31914
+ */
 import apiFetch from '@wordpress/api-fetch';
-import { dispatch, useSelect } from '@wordpress/data';
+import { dispatch, select, subscribe } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import { registerPlugin } from '@wordpress/plugins';
 import { addQueryArgs } from '@wordpress/url';
 
 const { classifaiDalleData } = window;
 
-const RegisterInserterMediaCategory = () => {
-	const { inserterMediaCategories } = useSelect(
-		( select ) => select( 'core/block-editor' ).getSettings(),
-		[]
-	);
+const isInserterOpened = () =>
+	select( 'core/edit-post' )?.isInserterOpened() ||
+	select( 'core/edit-site' )?.isInserterOpened() ||
+	select( 'core/edit-widgets' )?.isInserterOpened?.();
 
-	// If we have no categories yet, assume things aren't ready yet.
-	if ( ! inserterMediaCategories ) {
-		return null;
-	}
-
-	// If our custom category is already registered, don't register it again.
-	if (
-		inserterMediaCategories.some(
-			( { name } ) => name === 'classifai-generate-image'
-		)
-	) {
-		return null;
-	}
-
-	// Ensure the function we want to use is available.
-	if (
-		typeof dispatch( 'core/block-editor' ).registerInserterMediaCategory !==
-		'function'
-	) {
-		return null;
-	}
-
-	dispatch( 'core/block-editor' ).registerInserterMediaCategory( {
-		name: 'classifai-generate-image',
-		labels: {
-			name: classifaiDalleData.tabText,
-			search_items: __( 'Enter a prompt', 'classifai' ),
-		},
-		mediaType: 'image',
-		fetch: async ( { search = '' } ) => {
-			if ( ! search ) {
-				return [];
+const waitFor = async ( selector ) =>
+	new Promise( ( resolve ) => {
+		const unsubscribe = subscribe( () => {
+			if ( selector() ) {
+				unsubscribe();
+				resolve();
 			}
-
-			const images = await apiFetch( {
-				path: addQueryArgs( classifaiDalleData.endpoint, {
-					prompt: search,
-					format: 'b64_json',
-				} ),
-				method: 'GET',
-			} )
-				.then( ( response ) =>
-					response.map( ( item ) => ( {
-						title: search,
-						url: `data:image/png;base64,${ item.url }`,
-						previewUrl: `data:image/png;base64,${ item.url }`,
-						id: undefined,
-						alt: search,
-						caption: classifaiDalleData.caption,
-					} ) )
-				)
-				.catch( () => [] );
-
-			return images;
-		},
-		isExternalResource: true,
+		} );
 	} );
 
-	return null;
-};
+waitFor( isInserterOpened ).then( () =>
+	dispatch( 'core/block-editor' )?.registerInserterMediaCategory?.(
+		registerGenerateImageMediaCategory()
+	)
+);
 
-registerPlugin( 'classifai-inserter-media-category', {
-	render: RegisterInserterMediaCategory,
+const registerGenerateImageMediaCategory = () => ( {
+	name: 'classifai-generate-image',
+	labels: {
+		name: classifaiDalleData.tabText,
+		search_items: __( 'Enter a prompt', 'classifai' ),
+	},
+	mediaType: 'image',
+	fetch: async ( { search = '' } ) => {
+		if ( ! search ) {
+			return [];
+		}
+
+		const images = await apiFetch( {
+			path: addQueryArgs( classifaiDalleData.endpoint, {
+				prompt: search,
+				format: 'b64_json',
+			} ),
+			method: 'GET',
+		} )
+			.then( ( response ) =>
+				response.map( ( item ) => ( {
+					title: search,
+					url: `data:image/png;base64,${ item.url }`,
+					previewUrl: `data:image/png;base64,${ item.url }`,
+					id: undefined,
+					alt: search,
+					caption: classifaiDalleData.caption,
+				} ) )
+			)
+			.catch( () => [] );
+
+		return images;
+	},
+	isExternalResource: true,
 } );
