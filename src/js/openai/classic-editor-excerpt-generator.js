b33ebd47a55b8bd8b267a27a059e2902473804a8
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-08-10 22:49:29 +0530

    format: resole eslint issues

--- src/js/openai/classic-editor-excerpt-generator.js
+++ src/js/openai/classic-editor-excerpt-generator.js
@@ -1,58 +1,79 @@
-
-/* Variables */
+/* globals classifaiGenerateExcerpt */
 const classifaiGenerateExcerptButtonID = 'classifai-generate-excerpt';
-const classifaiGenerateExcerptButtonElement = document.getElementById(classifaiGenerateExcerptButtonID);
+const classifaiGenerateExcerptButtonElement = document.getElementById(
+	classifaiGenerateExcerptButtonID
+);
 const classifaiGenerateExcerptTextareaID = 'excerpt';
-const classifaiGenerateExcerptTextareaElement = document.getElementById(classifaiGenerateExcerptTextareaID);
+const classifaiGenerateExcerptTextareaElement = document.getElementById(
+	classifaiGenerateExcerptTextareaID
+);
 let classifarGenerateExcerptDebug = false;
 let classifaiGenerateExcerptText;
 
 // Note: classifarGenerateExcerptDebug is set by the SCRIPT_DEBUG constant
 // Errors are only logged to console when SCRIPT_DEBUG is defined with value: TRUE
-if (classifaiGenerateExcerpt && classifaiGenerateExcerpt.scriptDebug) {
+if ( classifaiGenerateExcerpt && classifaiGenerateExcerpt.scriptDebug ) {
 	classifarGenerateExcerptDebug = classifaiGenerateExcerpt.scriptDebug;
 }
 
 /* Generate excerpt when the button is clicked */
-if (null !== classifaiGenerateExcerptButtonElement) {
-
+if ( null !== classifaiGenerateExcerptButtonElement ) {
 	classifaiGenerateExcerptButtonElement.onclick = function () {
-		classifarGenerateExcerptDebug && console.log('Classifai Generate Excerpt Debug: Button clicked, excerpt is being generated..');
+		if ( classifarGenerateExcerptDebug ) {
+			// eslint-disable-next-line no-console
+			console.log(
+				'Classifai Generate Excerpt Debug: Button clicked, excerpt is being generated..'
+			);
+		}
+
 		classifaiExcerptGenerate();
 	};
 }
 
 /* Generate excerpt from API endpoint */
 function classifaiExcerptGenerate() {
-
 	// Confirm the endpoint URL is available; excerpt generation cannot function without this.
-	if (!classifaiGenerateExcerpt || false === classifaiGenerateExcerpt.endpointUrl) {
-		classifarGenerateExcerptDebug && console.log('Classifai Generate Excerpt Debug: Endpoint URL not set!');
+	if (
+		! classifaiGenerateExcerpt ||
+		false === classifaiGenerateExcerpt.endpointUrl
+	) {
+		if ( classifarGenerateExcerptDebug ) {
+			// eslint-disable-next-line no-console
+			console.log(
+				'Classifai Generate Excerpt Debug: Endpoint URL not set!'
+			);
+		}
+
 		return;
 	}
 
 	classifaiGenerateExcerptButtonElement.disabled = true;
 
-	fetch(classifaiGenerateExcerpt.endpointUrl, {
+	fetch( classifaiGenerateExcerpt.endpointUrl, {
 		headers: {
-			'X-WP-Nonce': classifaiGenerateExcerpt.nonce
-		}
-	})
-		.then((response) => response.json())
-		.then((result) => {
+			'X-WP-Nonce': classifaiGenerateExcerpt.nonce,
+		},
+	} )
+		.then( ( response ) => response.json() )
+		.then( ( result ) => {
 			classifaiGenerateExcerptText = result;
 
-			classifarGenerateExcerptDebug && console.log('Classifai Generate Excerpt Debug: Generated Text', classifaiGenerateExcerptText)
-
-			if (classifaiGenerateExcerptText) {
-				classifaiGenerateExcerptTextareaElement.textContent = classifaiGenerateExcerptText
-				classifaiGenerateExcerptButtonElement.value = classifaiGenerateExcerpt.regenerateExcerptText;
+			if ( classifarGenerateExcerptDebug ) {
+				// eslint-disable-next-line no-console
+				console.log(
+					'Classifai Generate Excerpt Debug: Generated Text',
+					classifaiGenerateExcerptText
+				);
 			}
-		})
-		.catch((error) => {
-		})
-		.finally(() => {
-			classifaiGenerateExcerptButtonElement.disabled = false
-		});
 
+			if ( classifaiGenerateExcerptText ) {
+				classifaiGenerateExcerptTextareaElement.textContent =
+					classifaiGenerateExcerptText;
+				classifaiGenerateExcerptButtonElement.value =
+					classifaiGenerateExcerpt.regenerateExcerptText;
+			}
+		} )
+		.finally( () => {
+			classifaiGenerateExcerptButtonElement.disabled = false;
+		} );
 }
