f1f3623ef6562445c15d53ae3d176506253093d8
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-10-20 13:09:01 +0530

    format: improve code formatting

--- src/js/admin.js
+++ src/js/admin.js
@@ -6,7 +6,7 @@ import 'tippy.js/themes/light.css';
 
 document.addEventListener( 'DOMContentLoaded', function () {
 	const template = document.getElementById( 'help-menu-template' );
-	if ( !template ) {
+	if ( ! template ) {
 		return;
 	}
 	const container = document.createElement( 'div' );
@@ -19,180 +19,198 @@ document.addEventListener( 'DOMContentLoaded', function () {
 		placement: 'bottom-end',
 		arrow: true,
 		animation: 'scale',
-		duration: [250, 200],
+		duration: [ 250, 200 ],
 		theme: 'light',
 		interactive: true,
 	} );
 } );
 
-(
-	() => {
-		const $toggler = document.getElementById( 'classifai-waston-cred-toggle' );
-		const $userField = document.getElementById(
-			'classifai-settings-watson_username'
-		);
+( () => {
+	const $toggler = document.getElementById( 'classifai-waston-cred-toggle' );
+	const $userField = document.getElementById(
+		'classifai-settings-watson_username'
+	);
 
-		if ( $toggler === null || $userField === null ) {
+	if ( $toggler === null || $userField === null ) {
+		return;
+	}
+
+	let $userFieldWrapper = null;
+	let $passwordFieldTitle = null;
+	if ( $userField.closest( 'tr' ) ) {
+		$userFieldWrapper = $userField.closest( 'tr' );
+	} else if ( $userField.closest( '.classifai-setup-form-field' ) ) {
+		$userFieldWrapper = $userField.closest( '.classifai-setup-form-field' );
+	}
+
+	if (
+		document
+			.getElementById( 'classifai-settings-watson_password' )
+			.closest( 'tr' )
+	) {
+		[ $passwordFieldTitle ] = document
+			.getElementById( 'classifai-settings-watson_password' )
+			.closest( 'tr' )
+			.getElementsByTagName( 'label' );
+	} else if (
+		document
+			.getElementById( 'classifai-settings-watson_password' )
+			.closest( '.classifai-setup-form-field' )
+	) {
+		[ $passwordFieldTitle ] = document
+			.getElementById( 'classifai-settings-watson_password' )
+			.closest( '.classifai-setup-form-field' )
+			.getElementsByTagName( 'label' );
+	}
+
+	$toggler.addEventListener( 'click', ( e ) => {
+		e.preventDefault();
+		$userFieldWrapper.classList.toggle( 'hidden' );
+
+		if ( $userFieldWrapper.classList.contains( 'hidden' ) ) {
+			$toggler.innerText = ClassifAI.use_password;
+			$passwordFieldTitle.innerText = ClassifAI.api_key;
+			$userField.value = 'apikey';
 			return;
 		}
 
-		let $userFieldWrapper = null;
-		let $passwordFieldTitle = null;
-		if ( $userField.closest( 'tr' ) ) {
-			$userFieldWrapper = $userField.closest( 'tr' );
-		} else if ( $userField.closest( '.classifai-setup-form-field' ) ) {
-			$userFieldWrapper = $userField.closest( '.classifai-setup-form-field' );
-		}
+		$toggler.innerText = ClassifAI.use_key;
+		$passwordFieldTitle.innerText = ClassifAI.api_password;
+	} );
+} )();
 
-		if (
-			document
-				.getElementById( 'classifai-settings-watson_password' )
-				.closest( 'tr' )
-		) {
-			[$passwordFieldTitle] = document
-				.getElementById( 'classifai-settings-watson_password' )
-				.closest( 'tr' )
-				.getElementsByTagName( 'label' );
-		} else if (
-			document
-				.getElementById( 'classifai-settings-watson_password' )
-				.closest( '.classifai-setup-form-field' )
-		) {
-			[$passwordFieldTitle] = document
-				.getElementById( 'classifai-settings-watson_password' )
-				.closest( '.classifai-setup-form-field' )
-				.getElementsByTagName( 'label' );
-		}
+( () => {
+	// Attach event to add new prompt button.
+	const $addNewPromptFieldsetButton = document.querySelectorAll(
+		'button.js-classifai-add-prompt-fieldset'
+	);
+	if ( $addNewPromptFieldsetButton.length ) {
+		$addNewPromptFieldsetButton.forEach( ( button ) => {
+			button.addEventListener( 'click', ( e ) => {
+				e.preventDefault();
 
-		$toggler.addEventListener( 'click', ( e ) => {
+				addNewFieldSet( e.target.previousElementSibling );
+			} );
+		} );
+	}
+
+	// Attach event to existing prompt fieldsets.
+	const $promptFieldsets = document.querySelectorAll(
+		'.classifai-field-type-prompt-setting'
+	);
+	if ( $promptFieldsets.length ) {
+		$promptFieldsets.forEach( ( $promptFieldset ) => {
+			attachEventPromptFieldset( $promptFieldset );
+		} );
+	}
+
+	// ------------------
+	// Helper function
+	// ------------------
+
+	/**
+	 * Reset all input fields in a fieldset.
+	 *
+	 * @since x.x.x
+	 * @param {Element} $fieldset
+	 * @param {Element} $parentRow
+	 */
+	function resetInputFields( $fieldset, $parentRow ) {
+		const $allFieldsets = $parentRow.querySelectorAll( 'fieldset' );
+		const $lastFieldset = Array.from( $allFieldsets ).pop();
+		const highestFieldIndexOfFieldset = parseInt(
+			$lastFieldset.querySelector( 'input' ).name.match( /\d+/ ).pop()
+		);
+		const fields = $fieldset.querySelectorAll( 'input, textarea' );
+		const actionButtons = $fieldset.querySelectorAll(
+			'.actions-rows .action__set_default'
+		);
+
+		// Reset form fields.
+		fields.forEach( ( field ) => {
+			field.value = '';
+
+			// Add index to field name.
+			field.name = field.name.replace(
+				/(\d+)/g,
+				() => highestFieldIndexOfFieldset + 1
+			);
+		} );
+
+		// Reset action buttons.
+		actionButtons.forEach( ( button ) => {
+			button.classList.remove( 'selected' );
+		} );
+	}
+
+	/**
+	 * Attach event to fieldset.
+	 *
+	 * @since x.x.x
+	 * @param {Element} $newPromptFieldset
+	 */
+	function attachEventPromptFieldset( $newPromptFieldset ) {
+		// Add event to remove button.
+		const $removePromptFieldsetButton = $newPromptFieldset.querySelector(
+			'.action__remove_prompt'
+		);
+		$removePromptFieldsetButton.addEventListener( 'click', ( e ) => {
 			e.preventDefault();
-			$userFieldWrapper.classList.toggle( 'hidden' );
+			e.target.closest( 'fieldset' ).remove();
+		} );
 
-			if ( $userFieldWrapper.classList.contains( 'hidden' ) ) {
-				$toggler.innerText = ClassifAI.use_password;
-				$passwordFieldTitle.innerText = ClassifAI.api_key;
-				$userField.value = 'apikey';
+		// Add event to set as default button.
+		const $setAsDefaultButton = $newPromptFieldset.querySelector(
+			'.action__set_default'
+		);
+		$setAsDefaultButton.addEventListener( 'click', ( e ) => {
+			e.preventDefault();
+
+			// If already selected, do nothing.
+			if ( e.target.classList.contains( 'selected' ) ) {
 				return;
 			}
 
-			$toggler.innerText = ClassifAI.use_key;
-			$passwordFieldTitle.innerText = ClassifAI.api_password;
+			// Remove selected class from all buttons.
+			const $settingRow = e.target.closest( 'tr' );
+			const $setAsDefaultButtons = $settingRow.querySelectorAll(
+				'.action__set_default'
+			);
+			$setAsDefaultButtons.forEach( ( button ) => {
+				button.classList.remove( 'selected' );
+				button
+					.closest( 'fieldset' )
+					.querySelector( 'input[name="default"]' ).value = '';
+			} );
+
+			// Set selected class.
+			e.target.classList.add( 'selected' );
+
+			// Set default value.
+			$newPromptFieldset.querySelector( 'input[name="default"]' ).value =
+				'true';
 		} );
 	}
-)();
 
-(
-	() => {
-		// Attach event to add new prompt button.
-		const $addNewPromptFieldsetButton = document.querySelectorAll( 'button.js-classifai-add-prompt-fieldset' );
-		if ( $addNewPromptFieldsetButton.length ) {
-			$addNewPromptFieldsetButton.forEach( ( button ) => {
-				button.addEventListener( 'click', ( e ) => {
-					e.preventDefault();
+	/**
+	 * Add a new fieldset.
+	 *
+	 * @since x.x.x
+	 * @param {Element} $sibling
+	 *
+	 * @return {Element} $newPromptFieldset
+	 */
+	function addNewFieldSet( $sibling ) {
+		const $promptFieldsetTemplate = $sibling.parentElement.querySelector(
+			'.classifai-field-type-prompt-setting'
+		);
+		const $newPromptFieldset = $promptFieldsetTemplate.cloneNode( true );
 
-					addNewFieldSet( e.target.previousElementSibling );
-				} );
-			} );
-		}
+		resetInputFields( $newPromptFieldset, $sibling.closest( 'tr' ) );
+		attachEventPromptFieldset( $newPromptFieldset );
 
-		// Attach event to existing prompt fieldsets.
-		const $promptFieldsets = document.querySelectorAll( '.classifai-field-type-prompt-setting' );
-		if ( $promptFieldsets.length ) {
-			$promptFieldsets.forEach( ( $promptFieldset ) => {
-				attachEventPromptFieldset( $promptFieldset );
-			} );
-		}
+		$sibling.insertAdjacentElement( 'afterend', $newPromptFieldset );
 
-		// ------------------
-		// Helper function
-		// ------------------
-
-		/**
-		 * Reset all input fields in a fieldset.
-		 *
-		 * @since x.x.x
-		 * @param {Element} $fieldset
-		 * @param {Element} $parentRow
-		 */
-		function resetInputFields( $fieldset, $parentRow ) {
-			const $allFieldsets = $parentRow.querySelectorAll('fieldset');
-			const $lastFieldset = Array.from($allFieldsets).pop();
-			const highestFieldIndexOfFieldset = parseInt($lastFieldset.querySelector('input').name.match(/\d+/).pop());
-			const fields = $fieldset.querySelectorAll( 'input, textarea' );
-			const actionButtons = $fieldset.querySelectorAll( '.actions-rows .action__set_default' );
-
-			// Reset form fields.
-			fields.forEach( ( field ) => {
-				field.value = '';
-
-				// Add index to field name.
-				field.name = field.name.replace( /(\d+)/g, () => highestFieldIndexOfFieldset + 1 );
-			} );
-
-			// Reset action buttons.
-			actionButtons.forEach( ( button ) => {
-				button.classList.remove( 'selected' );
-			} );
-		}
-
-		/**
-		 * Attach event to fieldset.
-		 *
-		 * @since x.x.x
-		 * @param {Element} $newPromptFieldset
-		 */
-		function attachEventPromptFieldset( $newPromptFieldset ) {
-			// Add event to remove button.
-			const $removePromptFieldsetButton = $newPromptFieldset.querySelector( '.action__remove_prompt' );
-			$removePromptFieldsetButton.addEventListener( 'click', ( e ) => {
-				e.preventDefault();
-				e.target.closest( 'fieldset' ).remove();
-			} );
-
-			// Add event to set as default button.
-			const $setAsDefaultButton = $newPromptFieldset.querySelector( '.action__set_default' );
-			$setAsDefaultButton.addEventListener( 'click', ( e ) => {
-				e.preventDefault();
-
-				// If already selected, do nothing.
-				if( e.target.classList.contains( 'selected' ) ) {
-					return;
-				}
-
-				// Remove selected class from all buttons.
-				const $settingRow = e.target.closest( 'tr' );
-				const $setAsDefaultButtons = $settingRow.querySelectorAll( '.action__set_default' );
-				$setAsDefaultButtons.forEach( ( button ) => {
-					button.classList.remove( 'selected' );
-					button.closest('fieldset').querySelector('input[name="default"]').value = '';
-				});
-
-				// Set selected class.
-				e.target.classList.add( 'selected' );
-
-				// Set default value.
-				$newPromptFieldset.querySelector('input[name="default"]').value = 'true';
-			} );
-		}
-
-		/**
-		 * Add a new fieldset.
-		 *
-		 * @since x.x.x
-		 * @param {Element} $sibling
-		 *
-		 * @returns {Element} $newPromptFieldset
-		 */
-		function addNewFieldSet( $sibling ) {
-			const $promptFieldsetTemplate = $sibling.parentElement.querySelector( '.classifai-field-type-prompt-setting' );
-			const $newPromptFieldset = $promptFieldsetTemplate.cloneNode( true );
-
-			resetInputFields( $newPromptFieldset, $sibling.closest('tr') );
-			attachEventPromptFieldset( $newPromptFieldset );
-
-			$sibling.insertAdjacentElement( 'afterend', $newPromptFieldset );
-
-			return $newPromptFieldset;
-		}
+		return $newPromptFieldset;
 	}
-)();
+} )();
