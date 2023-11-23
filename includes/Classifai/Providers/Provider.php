3d83511c3b0b23325beab7b1965aecc626a554dc
faisal-alvi <faisalalvi33@gmail.com>
2023-11-23 14:10:33 +0530

    render threshold fields

--- includes/Classifai/Providers/Provider.php
+++ includes/Classifai/Providers/Provider.php
@@ -404,10 +404,12 @@ abstract class Provider {
 		foreach ( $args['options'] as $option_value => $option_label ) {
 			$value       = '';
 			$default_key = array_search( $option_value, $args['default_values'], true );
+			$option_value_theshold = $option_value . '_threshold';
 
 			// Get saved value, if any.
 			if ( isset( $setting_index[ $args['label_for'] ] ) ) {
 				$value = $setting_index[ $args['label_for'] ][ $option_value ] ?? '';
+				$threshold_value = $setting_index[ $args['label_for'] ][ $option_value_theshold ] ?? '';
 			}
 
 			// Check for backward compatibility.
@@ -435,6 +437,11 @@ abstract class Provider {
 				checked( $value, $option_value, false ),
 				esc_html( $option_label )
 			);
+
+			// Render Threshold field.
+			if ( 'openai_embeddings' === $this->option_name && 'taxonomies' === $args['label_for'] ) {
+				$this->render_threshold_field( $args, $option_value_theshold, $threshold_value );
+			}
 		}
 
 		// Render description, if any.
@@ -445,6 +452,35 @@ abstract class Provider {
 			);
 		}
 	}
+
+	/**
+	 * Render a threshold field.
+	 *
+	 * @since 2.6.0
+	 *
+	 * @param array  $args          The args passed to add_settings_field
+	 * @param string $option_value  The option value.
+	 * @param string $value         The value.
+	 *
+	 * @return void
+	 */
+	public function render_threshold_field( $args, $option_value, $value ) {
+		printf(
+			'<p>
+				<label for="%1$s_%2$s_%3$s">
+					<input type="number" id="%1$s_%2$s_%3$s" name="classifai_%1$s[%2$s][%3$s]" value="%4$s" />
+					%5$s
+				</label>
+			</p>',
+			esc_attr( $this->option_name ),
+			esc_attr( $args['label_for'] ),
+			esc_attr( $option_value ),
+			esc_attr( $value ),
+			esc_html__( 'Threshold', 'classifai' )
+		);
+	}
+
+
 
 	/**
 	 * Renders the checkbox group for 'Generate descriptive text' setting.
