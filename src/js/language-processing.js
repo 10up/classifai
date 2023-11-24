cd1862d04cf610d6d321a994ca222c543d406b88
faisal-alvi <faisalalvi33@gmail.com>
2023-11-24 21:52:34 +0530

    handle the client side actions

--- src/js/language-processing.js
+++ src/js/language-processing.js
@@ -2,121 +2,130 @@ import Choices from 'choices.js';
 import '../scss/language-processing.scss';
 
 ( () => {
-	const nonceElement = document.getElementById( 'classifai-previewer-nonce' );
-	if ( ! nonceElement ) {
-		return;
-	}
 
-	/** Previewer nonce. */
-	const previewerNonce = nonceElement.value;
+	let featureStatuses = {};
 
-	/** Feature statuses. */
-	const featureStatuses = {
-		categoriesStatus: document.getElementById(
-			'classifai-settings-category'
-		).checked,
-		keywordsStatus: document.getElementById( 'classifai-settings-keyword' )
-			.checked,
-		entitiesStatus: document.getElementById( 'classifai-settings-entity' )
-			.checked,
-		conceptsStatus: document.getElementById( 'classifai-settings-concept' )
-			.checked,
-	};
+	const previewWatson = () => {
+		const nonceElement = document.getElementById( 'classifai-previewer-watson_nlu-nonce' );
+		if ( ! nonceElement ) {
+			return;
+		}
 
-	const plurals = {
-		category: 'categories',
-		keyword: 'keywords',
-		entity: 'entities',
-		concept: 'concepts',
-	};
-
-	document
-		.querySelectorAll(
-			'#classifai-settings-category, #classifai-settings-keyword, #classifai-settings-entity, #classifai-settings-concept'
-		)
-		.forEach( ( item ) => {
-			item.addEventListener( 'change', ( e ) => {
-				if ( 'classifai-settings-category' === e.target.id ) {
-					featureStatuses.categoriesStatus = e.target.checked;
-				}
-
-				if ( 'classifai-settings-keyword' === e.target.id ) {
-					featureStatuses.keywordsStatus = e.target.checked;
-				}
-
-				if ( 'classifai-settings-entity' === e.target.id ) {
-					featureStatuses.entitiesStatus = e.target.checked;
-				}
-
-				if ( 'classifai-settings-concept' === e.target.id ) {
-					featureStatuses.conceptsStatus = e.target.checked;
-				}
-
-				const taxType = e.target.id.split( '-' ).at( -1 );
-
-				if ( e.target.checked ) {
-					document
-						.querySelector( `.tax-row--${ plurals[ taxType ] }` )
-						.classList.remove( 'tax-row--hide' );
-				} else {
-					document
-						.querySelector( `.tax-row--${ plurals[ taxType ] }` )
-						.classList.add( 'tax-row--hide' );
-				}
-			} );
-		} );
-
-	/**
-	 * Live preview features.
-	 *
-	 * @param {Object} e The event object.
-	 */
-	function showPreview( e ) {
-		/** Category thresholds. */
-		const categoryThreshold = Number(
-			document.querySelector( '#classifai-settings-category_threshold' )
-				.value
+		const getClassifierDataBtn = document.getElementById(
+			'get-classifier-preview-data-btn'
 		);
-		const keywordThreshold = Number(
-			document.querySelector( '#classifai-settings-keyword_threshold' )
-				.value
-		);
-		const entityThreshold = Number(
-			document.querySelector( '#classifai-settings-entity_threshold' )
-				.value
-		);
-		const conceptThreshold = Number(
-			document.querySelector( '#classifai-settings-concept_threshold' )
-				.value
-		);
+		getClassifierDataBtn.addEventListener( 'click', showPreviewWatson );
 
-		const postId = document.getElementById(
-			'classifai-preview-post-selector'
-		).value;
+		/** Previewer nonce. */
+		const previewerNonce = nonceElement.value;
 
-		const previewWrapper = document.getElementById(
-			'classifai-post-preview-wrapper'
-		);
-		const thresholds = {
-			categories: categoryThreshold,
-			keywords: keywordThreshold,
-			entities: entityThreshold,
-			concepts: conceptThreshold,
+		/** Feature statuses. */
+		featureStatuses = {
+			categoriesStatus: document.getElementById(
+				'classifai-settings-category'
+			).checked,
+			keywordsStatus: document.getElementById( 'classifai-settings-keyword' )
+				.checked,
+			entitiesStatus: document.getElementById( 'classifai-settings-entity' )
+				.checked,
+			conceptsStatus: document.getElementById( 'classifai-settings-concept' )
+				.checked,
 		};
 
-		e.target
-			.closest( '.button' )
-			.classList.add( 'get-classifier-preview-data-btn--loading' );
+		const plurals = {
+			category: 'categories',
+			keyword: 'keywords',
+			entity: 'entities',
+			concept: 'concepts',
+		};
 
-		const formData = new FormData();
-		formData.append( 'action', 'get_post_classifier_preview_data' );
-		formData.append( 'post_id', postId );
-		formData.append( 'nonce', previewerNonce );
+		document
+			.querySelectorAll(
+				'#classifai-settings-category, #classifai-settings-keyword, #classifai-settings-entity, #classifai-settings-concept'
+			)
+			.forEach( ( item ) => {
+				item.addEventListener( 'change', ( e ) => {
+					if ( 'classifai-settings-category' === e.target.id ) {
+						featureStatuses.categoriesStatus = e.target.checked;
+					}
 
-		fetch( `${ ajaxurl }`, {
-			method: 'POST',
-			body: formData,
-		} )
+					if ( 'classifai-settings-keyword' === e.target.id ) {
+						featureStatuses.keywordsStatus = e.target.checked;
+					}
+
+					if ( 'classifai-settings-entity' === e.target.id ) {
+						featureStatuses.entitiesStatus = e.target.checked;
+					}
+
+					if ( 'classifai-settings-concept' === e.target.id ) {
+						featureStatuses.conceptsStatus = e.target.checked;
+					}
+
+					const taxType = e.target.id.split( '-' ).at( -1 );
+
+					if ( e.target.checked ) {
+						document
+							.querySelector( `.tax-row--${ plurals[ taxType ] }` )
+							.classList.remove( 'tax-row--hide' );
+					} else {
+						document
+							.querySelector( `.tax-row--${ plurals[ taxType ] }` )
+							.classList.add( 'tax-row--hide' );
+					}
+				} );
+			} );
+
+		/**
+		 * Live preview features.
+		 *
+		 * @param {Object} e The event object.
+		 */
+		function showPreviewWatson( e ) {
+			/** Category thresholds. */
+			const categoryThreshold = Number(
+				document.querySelector( '#classifai-settings-category_threshold' )
+					.value
+			);
+			const keywordThreshold = Number(
+				document.querySelector( '#classifai-settings-keyword_threshold' )
+					.value
+			);
+			const entityThreshold = Number(
+				document.querySelector( '#classifai-settings-entity_threshold' )
+					.value
+			);
+			const conceptThreshold = Number(
+				document.querySelector( '#classifai-settings-concept_threshold' )
+					.value
+			);
+
+			const postId = document.getElementById(
+				'classifai-preview-post-selector'
+			).value;
+
+			const previewWrapper = document.getElementById(
+				'classifai-post-preview-wrapper'
+			);
+			const thresholds = {
+				categories: categoryThreshold,
+				keywords: keywordThreshold,
+				entities: entityThreshold,
+				concepts: conceptThreshold,
+			};
+
+			e.target
+				.closest( '.button' )
+				.classList.add( 'get-classifier-preview-data-btn--loading' );
+
+			const formData = new FormData();
+			formData.append( 'action', 'get_post_classifier_preview_data' );
+			formData.append( 'post_id', postId );
+			formData.append( 'nonce', previewerNonce );
+
+			fetch( `${ ajaxurl }`, {
+				method: 'POST',
+				body: formData,
+			} )
 			.then( ( response ) => {
 				return response.json();
 			} )
@@ -162,33 +171,113 @@ import '../scss/language-processing.scss';
 						'get-classifier-preview-data-btn--loading'
 					);
 			} );
-	}
+		}
 
-	/**
-	 * Filters response data depending on the threshold value.
-	 *
-	 * @param {Object} data       Response data from NLU.
-	 * @param {Object} thresholds Object containing threshold values for various taxnomy types.
-	 * @return {Array} Sorted data.
-	 */
-	function filterByScoreOrRelevance( data = {}, thresholds ) {
-		const filteredItems = Object.keys( data ).map( ( key ) => ( {
-			[ key ]: data[ key ].filter( ( item ) => {
-				if ( item?.score && item.score * 100 > thresholds[ key ] ) {
+		/**
+		 * Filters response data depending on the threshold value.
+		 *
+		 * @param {Object} data       Response data from NLU.
+		 * @param {Object} thresholds Object containing threshold values for various taxnomy types.
+		 * @return {Array} Sorted data.
+		 */
+		function filterByScoreOrRelevance( data = {}, thresholds ) {
+			const filteredItems = Object.keys( data ).map( ( key ) => ( {
+				[ key ]: data[ key ].filter( ( item ) => {
+					if ( item?.score && item.score * 100 > thresholds[ key ] ) {
+						return item;
+					} else if (
+						item?.relevance &&
+						item.relevance * 100 > thresholds[ key ]
+					) {
+						return item;
+					}
+
 					return item;
-				} else if (
-					item?.relevance &&
-					item.relevance * 100 > thresholds[ key ]
-				) {
-					return item;
+				} ),
+			} ) );
+
+			return filteredItems;
+		}
+	};
+	previewWatson();
+
+	const previewEmbeddings = () => {
+		const nonceElement = document.getElementById( 'classifai-previewer-openai_embeddings-nonce' );
+		if ( ! nonceElement ) {
+			return;
+		}
+
+		const getClassifierDataBtn = document.getElementById(
+			'get-classifier-preview-data-btn'
+		);
+		getClassifierDataBtn.addEventListener( 'click', showPreviewEmeddings );
+
+		/** Previewer nonce. */
+		const previewerNonce = nonceElement.value;
+		
+		/**
+		 * Live preview features.
+		 *
+		 * @param {Object} e The event object.
+		 */
+		function showPreviewEmeddings( e ) {
+			const postId = document.getElementById(
+				'classifai-preview-post-selector'
+			).value;
+
+			const previewWrapper = document.getElementById(
+				'classifai-post-preview-wrapper'
+			);
+
+			// clear previewWrapper.
+			previewWrapper.innerHTML = '';
+
+			e.target
+				.closest( '.button' )
+				.classList.add( 'get-classifier-preview-data-btn--loading' );
+
+			const formData = new FormData();
+			formData.append( 'action', 'get_post_classifier_embeddings_preview_data' );
+			formData.append( 'post_id', postId );
+			formData.append( 'nonce', previewerNonce );
+
+			fetch( `${ ajaxurl }`, {
+				method: 'POST',
+				body: formData,
+			} )
+			.then( ( response ) => {
+				return response.json();
+			} )
+			.then( ( data ) => {
+				if ( ! data.success ) {
+					previewWrapper.style.display = 'block';
+					previewWrapper.innerHTML = data.data;
+					e.target
+						.closest( '.button' )
+						.classList.remove(
+							'get-classifier-preview-data-btn--loading'
+						);
+					return;
 				}
 
-				return item;
-			} ),
-		} ) );
+				const htmlData = buildPreviewUI( data.data );
+				previewWrapper.style.display = 'block';
+				previewWrapper.innerHTML = htmlData;
 
-		return filteredItems;
-	}
+				// remove all .tax-row--hide
+				document.querySelectorAll( '.tax-row--hide' ).forEach( ( item ) => {
+					item.classList.remove( 'tax-row--hide' );
+				} );
+				
+				e.target
+					.closest( '.button' )
+					.classList.remove(
+						'get-classifier-preview-data-btn--loading'
+					);
+			} );
+		}
+	};
+	previewEmbeddings();
 
 	/**
 	 * Builds user readable HTML data from the response by NLU.
@@ -198,7 +287,12 @@ import '../scss/language-processing.scss';
 	 */
 	function buildPreviewUI( filteredItems = [] ) {
 		let htmlData = '';
-		filteredItems.forEach( ( obj ) => {
+		// check if filteredItems.forEach type is function
+		if ( ! filteredItems.forEach ) {
+			return '';
+		}
+
+		filteredItems.forEach && filteredItems.forEach( ( obj ) => {
 			Object.keys( obj ).forEach( ( prop ) => {
 				htmlData += `<div class="tax-row tax-row--${ prop } ${
 					featureStatuses[ `${ prop }Status` ] ? '' : 'tax-row--hide'
@@ -237,11 +331,6 @@ import '../scss/language-processing.scss';
 
 		return htmlData;
 	}
-
-	const getClassifierDataBtn = document.getElementById(
-		'get-classifier-preview-data-btn'
-	);
-	getClassifierDataBtn.addEventListener( 'click', showPreview );
 
 	/*
 	 * Post selector Choices.js
