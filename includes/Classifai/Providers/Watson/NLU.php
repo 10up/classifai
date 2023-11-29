d14c9baa13dcddf3c89070c519279cb308413495
faisal-alvi <faisalalvi33@gmail.com>
2023-11-29 17:31:10 +0530

    improve the condition to load style in editor

--- includes/Classifai/Providers/Watson/NLU.php
+++ includes/Classifai/Providers/Watson/NLU.php
@@ -145,7 +145,7 @@ class NLU extends Provider {
 	 * Register what we need for the plugin.
 	 */
 	public function register() {
-		if ( isset( $_GET['page'] ) && 'classifai' === $_GET['page'] ) {
+		if ( $this->has_access( 'content_classification' ) || isset( $_GET['page'] ) && 'classifai' === $_GET['page'] ) {
 			add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_editor_assets' ] );
 			add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_admin_assets' ] );
 
