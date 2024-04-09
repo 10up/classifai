59d95da1203f02edb89cc0d7ce2e97b56411ae21
Darin Kotter <darin.kotter@gmail.com>
2024-04-09 17:15:00 -0600

    Remove lines of code that aren't used anymore

--- includes/Classifai/Helpers.php
+++ includes/Classifai/Helpers.php
@@ -5,7 +5,6 @@ namespace Classifai;
 use Classifai\Features\Classification;
 use Classifai\Providers\Provider;
 use Classifai\Admin\UserProfile;
-use Classifai\Providers\Watson\NLU;
 use Classifai\Providers\OpenAI\Embeddings;
 use Classifai\Services\Service;
 use Classifai\Services\ServicesManager;
