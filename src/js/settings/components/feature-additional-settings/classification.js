b0eb75cfd05ef94b85b0151ea66b1dde166904e7
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-08-31 17:08:13 +0530

    add previewer for IBMWatson

--- src/js/settings/components/feature-additional-settings/classification.js
+++ src/js/settings/components/feature-additional-settings/classification.js
@@ -339,6 +339,7 @@ function PreviewerResults() {
 	return (
 		<div className='classifai__classification-previewer-search-result-container'>
 			{ 'azure_openai_embeddings' === activeProvider || 'openai_embeddings' === activeProvider && <AzureOpenAIEmbeddingsResults postId={ selectedPostId } /> }
+			{ 'ibm_watson_nlu' === activeProvider && <IBMWatsonNLUResults postId={ selectedPostId } /> }
 		</div>
 	);
 }
@@ -411,7 +412,7 @@ function AzureOpenAIEmbeddingsResults( { postId } ) {
 
 			return (
 				<div className={ `classifai__classification-previewer-result-tag ${ scoreClass }` } key={ _index }>
-					<span className='classifai__classification-previewer-result-tag-score'>{ normalizeScore( tag.score ) }%</span>
+					<span className='classifai__classification-previewer-result-tag-score'>{ score }%</span>
 					<span className='classifai__classification-previewer-result-tag-label'>{ tag.label }</span>
 				</div>
 			)
@@ -425,7 +426,7 @@ function AzureOpenAIEmbeddingsResults( { postId } ) {
 					</Heading>
 				</CardHeader>
 				<CardBody>
-					{ tags.length ? tags : __( `No classification data found for ${ taxLabel }`, 'classifai' ) }
+					{ tags.length ? tags : __( `No classification data found for ${ taxLabel }.`, 'classifai' ) }
 				</CardBody>
 			</Card>
 		)
@@ -443,6 +444,123 @@ function AzureOpenAIEmbeddingsResults( { postId } ) {
 		<>
 			<Notice status='success' isDismissible={ false } className='classifai__classification-previewer-result-notice'>
 				{ __( 'Results for each taxonomy are sorted in descending order, starting with the term that has the highest score, indicating the best match based on the embedding data.', 'classifai' ) }
+			</Notice>
+			{ card }
+		</>
+	) : null
+}
+
+function IBMWatsonNLUResults( { postId } ) {
+	const {
+		isPreviewUnderProcess,
+		setPreviewUnderProcess,
+		setIsPreviewerOpen,
+	} = useContext( PreviewerProviderContext );
+
+	const [ responseData, setResponseData ] = useState( null );
+	const [ errorMessage, setErrorMessage ] = useState( '' );
+	const settings = useSelect( ( select ) => select( STORE_NAME ).getFeatureSettings() );
+
+	const taxMap = {
+		categories: 'category',
+		concepts: 'concept',
+		entities: 'entity',
+		keywords: 'keyword',
+	};
+
+	function formatLabel( label ) {
+		return label
+			.split( '/' )
+			.filter( ( i ) => '' !== i )
+			.join( ', ' );
+	}
+
+	useEffect( () => {
+		// Reset previous results.
+		if ( isPreviewUnderProcess ) {
+			setResponseData( null );
+		}
+	}, [ isPreviewUnderProcess ] );
+
+	useEffect( () => {
+		if ( ! postId ) {
+			return;
+		}
+
+		setPreviewUnderProcess( true );
+		setIsPreviewerOpen( true );
+
+		const formData = new FormData();
+
+		formData.append( 'action', 'get_post_classifier_preview_data' );
+		formData.append( 'post_id', postId );
+		formData.append( 'nonce', classifAISettings.nonce );
+
+		( async () => {
+			const response = await fetch( ajaxurl, {
+				method: 'POST',
+				body: formData,
+			} );
+
+			if ( ! response.ok ) {
+				return;
+			}
+
+			const responseJSON = await response.json();
+			if ( responseJSON.success ) {
+				setResponseData( responseJSON.data );
+			} else {
+				setErrorMessage( responseJSON.data );
+			}
+
+			setPreviewUnderProcess( false );
+		} )()
+	}, [] );
+
+	if ( ! responseData ) {
+		return null;
+	}
+
+	const renderData = {
+		categories: responseData.categories,
+		concepts: responseData.concepts,
+		entities: responseData.entities,
+		keywords: responseData.keywords,
+	};
+
+	const card = Object.keys( renderData ).map( ( taxSlug ) => {
+		const tags = renderData[ taxSlug ].map( ( tag, _index ) => {
+			const threshold = settings[ `${ taxMap[ taxSlug ] }_threshold` ];
+			const score = normalizeScore( tag.score || tag.relevance );
+
+			const scoreClass = score >= threshold ? 'classifai__classification-previewer-result-tag--exceeds-threshold' : '';
+
+			return (
+				<div className={ `classifai__classification-previewer-result-tag ${ scoreClass }` } key={ _index }>
+					<span className='classifai__classification-previewer-result-tag-score'>{ score }%</span>
+					<span className='classifai__classification-previewer-result-tag-label'>{ formatLabel( tag.label || tag.text ) }</span>
+				</div>
+			);
+		} );
+
+		return (
+			<Card className='classifai__classification-previewer-result-card' key={ taxSlug }>
+				<CardHeader>
+					<Heading className='classifai__classification-previewer-result-card-heading'>
+						{ taxSlug }
+					</Heading>
+				</CardHeader>
+				<CardBody>
+					{ tags.length ? tags : __( `No classification data found for ${ taxSlug }.`, 'classifai' ) }
+				</CardBody>
+			</Card>
+		)
+	} );
+
+	return card.length ? (
+		<>
+			<Notice status='success' isDismissible={ false } className='classifai__classification-previewer-result-notice'>
+				{ __( 'Results for each category are sorted in descending order, starting with the term that has the highest score, indicating the best match based on the embedding data.', 'classifai' ) }
 			</Notice>
 			{ card }
 		</>
