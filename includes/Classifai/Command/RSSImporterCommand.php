e8c14ff13e55e0c192cc95c5fd3c08afcf9b464f
Darin Kotter <darin.kotter@gmail.com>
2024-09-26 17:41:59 -0600

    Better handling for undefined constant

--- includes/Classifai/Command/RSSImporterCommand.php
+++ includes/Classifai/Command/RSSImporterCommand.php
@@ -200,8 +200,11 @@ class RSSImporterCommand extends \WP_CLI_Command {
 			$options['headers'] = [];
 		}
 
-		$options['headers']['x-api-key'] = defined( MERCURY_PARSER_API_KEY ) ?? ''; /** @phpstan-ignore constant.notFound (If constant not defined, will use empty string instead) */
-		$options['timeout']              = 60; // phpcs:ignore WordPressVIPMinimum.Performance.RemoteRequestTimeout.timeout_timeout
+		if ( defined( \MERCURY_PARSER_API_KEY ) && \MERCURY_PARSER_API_KEY ) {
+			$options['headers']['x-api-key'] = \MERCURY_PARSER_API_KEY;
+		}
+
+		$options['timeout'] = 60; // phpcs:ignore WordPressVIPMinimum.Performance.RemoteRequestTimeout.timeout_timeout
 
 		$request_url = 'https://mercury.postlight.com/parser?url=' . rawurlencode( $url );
 		$response    = wp_remote_get( $request_url, $options ); // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.wp_remote_get_wp_remote_get
