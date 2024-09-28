c805e5f130c269d89b55b1b33d02b5f7d33da190
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-09-28 23:10:29 +0530

    add classifai button to toolbar

--- src/js/features/slot-fill/index.js
+++ src/js/features/slot-fill/index.js
@@ -2,7 +2,15 @@
  * External dependencies
  */
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
-import { Icon, SlotFillProvider, Slot, Fill } from '@wordpress/components';
+import {
+	Icon,
+	SlotFillProvider,
+	Slot,
+	Fill,
+	Button,
+	Popover,
+} from '@wordpress/components';
+import { useEffect, useState, useRef } from '@wordpress/element';
 import { PluginArea, registerPlugin } from '@wordpress/plugins';
 import { __ } from '@wordpress/i18n';
 
@@ -39,3 +47,81 @@ registerPlugin( 'classifai-plugin-area', {
 window.ClassifaiEditorSettingPanel = ( { children } ) => {
 	return <Fill name="classifai-editor-setting-panel">{ children }</Fill>;
 };
+
+function ClassifaiButtonPlugin() {
+	const [ isReady, setIsReady ] = useState( false );
+	const [ isPopoverOpen, setIsPopoverOpen ] = useState( false );
+	const parentEl = useRef( null );
+	const buttonContainer = useRef( null );
+
+
+	useEffect( () => {
+		if ( buttonContainer.current ) {
+			return;
+		}
+
+		window._wpLoadBlockEditor.then( async () => {
+			if ( document.getElementById( 'classifai-toolbar-button' ) ) {
+				return null;
+			}
+
+			await new Promise( ( resolve ) => {
+				const intervalId = setInterval( () => {
+					parentEl.current = document.querySelector('.editor-header__settings');
+	
+					if ( parentEl.current && buttonContainer.current ) {
+						parentEl.current.insertBefore(
+							buttonContainer.current,
+							parentEl.current.firstChild
+						);
+					}
+
+					clearInterval( intervalId );
+					resolve();
+				}, 100 );
+			} );
+		} );
+
+		setIsReady( true );
+	} );
+
+	if ( ! isReady ) {
+		return null;
+	}
+
+	const popoverStyle = {
+		minWidth: '280px',
+		padding: '1rem',
+	}
+
+	return (
+		<div ref={ buttonContainer } id="classifai-toolbar-button">
+			<Button
+				icon={
+					<Icon
+						className="components-panel__icon"
+						icon={ icon }
+					/>
+				}
+				onClick={ () => setIsPopoverOpen( ! isPopoverOpen ) }
+			/>
+			{
+				isPopoverOpen && (
+					<Popover
+						anchor={ buttonContainer.current }
+						position="bottom"
+						onFocusOutside={ () => setIsPopoverOpen( ! isPopoverOpen ) }
+					>
+						<div style={ popoverStyle }>
+							<Slot name="classifai-editor-setting-panel" />
+						</div>
+					</Popover>
+				)
+			}
+		</div>
+	);
+}
+
+registerPlugin( 'classifai-plugin-area-v2', {
+	render: ClassifaiButtonPlugin,
+} );
