ec9dfc6dcb4d451392c3b6392ac9c6e7f04fc8e9
Darin Kotter <darin.kotter@gmail.com>
2025-02-11 13:12:18 -0700

    Fix typos

--- includes/Classifai/Providers/Localhost/OllamaEmbeddings.php
+++ includes/Classifai/Providers/Localhost/OllamaEmbeddings.php
@@ -761,7 +761,7 @@ class OllamaEmbeddings extends Ollama {
 	 *
 	 * @param string $taxonomy Taxonomy slug.
 	 * @param bool   $all      Whether to generate embeddings for all terms or just those without embeddings.
-	 * @param array  $args     Overrideable query args for get_terms()
+	 * @param array  $args     Overridable query args for get_terms()
 	 * @param int    $user_id  The user ID to run this as.
 	 */
 	public function trigger_taxonomy_update( string $taxonomy = '', bool $all = false, array $args = [], int $user_id = 0 ) {
@@ -848,7 +848,7 @@ class OllamaEmbeddings extends Ollama {
 	 *
 	 * @param string $taxonomy Taxonomy slug.
 	 * @param bool   $all      Whether to generate embeddings for all terms or just those without embeddings.
-	 * @param array  $args     Overrideable query args for get_terms()
+	 * @param array  $args     Overridable query args for get_terms()
 	 * @param int    $user_id  The user ID to run this as.
 	 */
 	public function generate_embedding_job( string $taxonomy = '', bool $all = false, array $args = [], int $user_id = 0 ) {
