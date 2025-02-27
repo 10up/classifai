9d48d0a90417f64438248b529331832547bc1373
Darin Kotter <darin.kotter@gmail.com>
2025-02-27 10:03:43 -0700

    Make prompt a bit more descriptive

--- includes/Classifai/Features/ContentGeneration.php
+++ includes/Classifai/Features/ContentGeneration.php
@@ -32,7 +32,7 @@ class ContentGeneration extends Feature {
 	 *
 	 * @var string
 	 */
-	public $prompt = 'Given the title and summary of an article, write a full length article to be used on a website. Only return the contents of the article, not the title. Separate each paragraph with double line breaks.'; // TODO: iterate on this to make it better.
+	public $prompt = 'Act as an experienced SEO copywriter tasked with writing an article based off of a given summary and an optionally provided title. Your goal is to craft a compelling, informative piece that adheres to SEO best practices, is well-researched, engaging to the target audience, and structured in a way that enhances readability. Incorporate relevant keywords naturally throughout the text, without compromising the flow or quality of the content. Ensure that the article provides value to the reader. Only return the contents of the article, not the title or other commentary. Separate each paragraph with double line breaks.';
 
 	/**
 	 * Constructor.
