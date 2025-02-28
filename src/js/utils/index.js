bb94e40086f71ae8eb348141545b903c5255ab82
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2025-02-28 17:46:36 +0530

    add replaceBlocksInSource util

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -14,6 +14,16 @@ export const filterAndFlattenAllowedBlocks = ( blocks = [], allowedBlocks = [] )
 );
 
 /**
+ * Removes the <!-- --> delimiters from the content.
+ *
+ * @param {Array} blocks Array of { clientId, content } objects.
+ * @return {Array} Array of objects with content without delimiters.
+ */
+export const removeBlockDelimiters = ( content ) => {
+	return content.replace( /<!--[\s\S]*?-->/g, '' );
+};
+
+/**
  * Retrieves the mapping of client IDs to block content.
  *
  * @param {Array} blocks
@@ -21,7 +31,7 @@ export const filterAndFlattenAllowedBlocks = ( blocks = [], allowedBlocks = [] )
  */
 export const getClientIdToBlockContentMapping = ( blocks = [] ) => blocks.map( ( block ) => ( {
 	clientId: block.clientId,
-	content: getBlockContent( block ),
+	content: removeBlockDelimiters( getBlockContent( block ) ),
 } ) );
 
 /**
@@ -30,7 +40,7 @@ export const getClientIdToBlockContentMapping = ( blocks = [] ) => blocks.map( (
  * @param {string} htmlContent HTML as string.
  * @return {string} HTML string without outermost tags stripped.
  */
-export function stripOutermostTag( htmlContent = '' ) {
+export const stripOutermostTag = ( htmlContent = '' ) => {
 	// Parse the input HTML string into a DOM structure
 	const parser = new DOMParser();
 	const doc = parser.parseFromString( htmlContent, 'text/html' );
@@ -40,4 +50,40 @@ export function stripOutermostTag( htmlContent = '' ) {
 
 	// Return the innerHTML of the outermost element, which removes the outermost tag
 	return outermostElement ? outermostElement.innerHTML : htmlContent;
+}
+
+
+/**
+ * Replaces subset of blocks in the copy of the editor's original blocks with
+ * modified blocks and returns a new array.
+ *
+ * Suppose the editor originally has 6 blocks and blocks 3 & 4 have undergone tone
+ * rewriting which returns blocks 3' and 4'. This function returns 1-2-3'-4'-5-6.
+ *
+ * @param {Array} sourceBlocks  Array of original, unmodified editor blocks.
+ * @param {Array} modifiedBlocks Subset of editor blocks which have undergone tone rewriting.
+ * @return {Array} Array of blocks that include original and modified blocks.
+ */
+export const replaceBlocksInSource = ( sourceBlocks = [], modifiedBlocks = [] ) => {
+	const updateBlock = ( blocks ) => {
+		return blocks.map( ( block ) => {
+			const modified = modifiedBlocks.find(
+				( modifiedBlock ) =>
+					modifiedBlock.clientId === block.clientId
+			);
+
+			if ( modified ) {
+				return modified.blocks[ 0 ];
+			}
+
+			return {
+				...block,
+				innerBlocks: block.innerBlocks
+					? updateBlock( block.innerBlocks )
+					: [],
+			};
+		} );
+	};
+
+	return updateBlock( sourceBlocks );
 }
