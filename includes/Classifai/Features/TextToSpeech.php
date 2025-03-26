80b9fda981916d700831b4fa60ad5085044f8cc8
Dharmesh Patel <dspatel44@gmail.com>
2025-03-26 15:16:01 +0530

    Ensure that the Text-to-Speech feature options are available only for the allowed post types.

--- includes/Classifai/Features/TextToSpeech.php
+++ includes/Classifai/Features/TextToSpeech.php
@@ -111,9 +111,11 @@ class TextToSpeech extends Feature {
 			return;
 		}
 
-		$post = get_post();
+		$post                 = get_post();
+		$supported_post_types = $this->get_supported_post_types();
 
-		if ( empty( $post ) ) {
+		// Only enqueue the script if we're on a supported post type.
+		if ( empty( $post ) || ! in_array( $post->post_type, $supported_post_types, true ) ) {
 			return;
 		}
 
