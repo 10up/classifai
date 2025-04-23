40f4fe6a0763d100ceec02b5b8bd8a4bded62cba
Darin Kotter <darin.kotter@gmail.com>
2025-04-23 16:43:43 -0600

    More E2E fixes

--- tests/cypress/integration/image-processing/image-generation-openai-dalle.test.js
+++ tests/cypress/integration/image-processing/image-generation-openai-dalle.test.js
@@ -15,7 +15,7 @@ describe( 'Image Generation (OpenAI DALL·E) Tests', () => {
 	it( 'Can save OpenAI "Image Processing" settings', () => {
 		cy.visitFeatureSettings( 'image_processing/feature_image_generation' );
 		cy.selectProvider( 'openai_dalle' );
-		cy.get( '#openai_dalle_api_key' ).clear().type( 'password' );
+		cy.get( '#openai_api_key' ).clear().type( 'password' );
 		cy.get( 'select#openai_dalle_number_of_images' ).select( '2' );
 		cy.get( 'select#openai_dalle_quality' ).select( 'low' );
 		cy.get( 'select#openai_dalle_image_size' ).select( '1024x1536' );
