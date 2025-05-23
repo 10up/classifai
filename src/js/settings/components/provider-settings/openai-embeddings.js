1b3d542697c2035feb0a349298807d402e19316e
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2025-05-23 21:21:57 +0530

    add some accessibility magic

--- src/js/settings/components/provider-settings/openai-embeddings.js
+++ src/js/settings/components/provider-settings/openai-embeddings.js
@@ -7,6 +7,7 @@ import {
 	Icon,
 	__experimentalInputControl as InputControl, // eslint-disable-line @wordpress/no-unsafe-wp-apis
 } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
@@ -45,11 +46,12 @@ export const OpenAIEmbeddingsSettings = ( { isConfigured = false } ) => {
 	const onChange = ( data ) => setProviderSettings( providerName, data );
 	const blockVariations = useSelect( select => select( coreStore ).getBlockVariations( 'core/query' ) );
 	const defaultTemplate = allSettings['default_template'];
+	const [ focusedTemplate, setFocusedTemplate ] = useState( defaultTemplate );
+	const [ isTemplateInFocus, setIsTemplateInFocus ] = useState( false );
 
 	const iconWrapperStyle = {
 		border: '1px solid #e0e0e0',
-		width: '100px',
-		padding: '0.5rem',
+		width: '120px',
 		borderRadius: '6px',
 		cursor: 'pointer',
 	}
@@ -81,11 +83,25 @@ export const OpenAIEmbeddingsSettings = ( { isConfigured = false } ) => {
 						<Flex align="normal" justify="start" gap={ 2 }>
 							{ blockVariations.map( variation => (
 								<FlexItem
+									role="button"
+									tabIndex="0"
+									onFocus={ () => {
+										setFocusedTemplate( variation.name );
+										setIsTemplateInFocus( true );
+									} }
+									onBlur={ () => setIsTemplateInFocus( false ) }
+									onKeyDown={ ( e ) => {
+										if ( 'Space' === e.code || 'Enter' === e.code) {
+											e.preventDefault();
+											setFeatureSettings({ default_template: variation.name });
+										}
+									}}
 									style={ {
 										...iconWrapperStyle,
 										borderColor: defaultTemplate === variation.name ? 'var(--wp-admin-theme-color)' : '#e0e0e0',
 										borderWidth: defaultTemplate === variation.name && '2px',
 										backgroundColor: defaultTemplate === variation.name && 'color-mix(in srgb, var(--wp-admin-theme-color) 10%, transparent)',
+										outline: isTemplateInFocus && focusedTemplate === variation.name && '2px solid var(--wp-admin-theme-color)',
 									} }
 									onClick={ () => setFeatureSettings( { default_template: variation.name } ) }
 								>
@@ -96,7 +112,7 @@ export const OpenAIEmbeddingsSettings = ( { isConfigured = false } ) => {
 											fill: 'var(--wp-admin-theme-color)',
 										} ) }
 									/>
-									<span style={ { display: "block" } }>{ variation.title }</span>
+									<span style={ { display: "block", padding: '0 10px 10px 10px' } }>{ variation.title }</span>
 								</FlexItem>
 							) ) }
 						</Flex>
