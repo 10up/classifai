98d5078472af7018da5a8dc51d813c9e09cebe72
faisal-alvi <faisalalvi33@gmail.com>
2025-06-26 21:50:14 +0530

    Enhance Excerpt Generation by including Author Context

--- includes/Classifai/Providers/OpenAI/ChatGPT.php
+++ includes/Classifai/Providers/OpenAI/ChatGPT.php
@@ -588,11 +588,36 @@ class ChatGPT extends Provider {
 
 		$feature   = new ExcerptGeneration();
 		$settings  = $feature->get_settings();
-		$args      = wp_parse_args(
+		
+		// Get author information if not provided
+		$author_name = '';
+		if ( ! isset( $args['author'] ) || empty( $args['author'] ) ) {
+			$post = get_post( $post_id );
+			if ( $post ) {
+				$author_name = get_the_author_meta( 'display_name', $post->post_author );
+				
+				/**
+				 * Filter the author name used in excerpt generation.
+				 *
+				 * @since 3.x.x
+				 * @hook classifai_excerpt_generation_author_name
+				 *
+				 * @param {string} $author_name The author's display name.
+				 * @param {int}    $post_id     The post ID.
+				 * @param {WP_Post} $post       The post object.
+				 *
+				 * @return {string} The author name to use in the prompt.
+				 */
+				$author_name = apply_filters( 'classifai_excerpt_generation_author_name', $author_name, $post_id, $post );
+			}
+		}
+		
+		$args = wp_parse_args(
 			array_filter( $args ),
 			[
 				'content' => '',
 				'title'   => get_the_title( $post_id ),
+				'author'  => $author_name,
 			]
 		);
 		$post_type = get_post_type( $post_id );
@@ -615,8 +640,8 @@ class ChatGPT extends Provider {
 		}
 
 		// Replace our variables in the prompt.
-		$prompt_search  = array( '{{WORDS}}', '{{TITLE}}' );
-		$prompt_replace = array( $excerpt_length, $args['title'] );
+		$prompt_search  = array( '{{WORDS}}', '{{TITLE}}', '{{AUTHOR}}' );
+		$prompt_replace = array( $excerpt_length, $args['title'], $args['author'] );
 		$prompt         = str_replace( $prompt_search, $prompt_replace, $excerpt_prompt );
 
 		/**
