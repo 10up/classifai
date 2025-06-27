c61e12f081cbd035f0cb420dd05536cd5efd1bc8
faisal-alvi <faisalalvi33@gmail.com>
2025-06-27 23:07:48 +0530

    Add commands to manage credential reuse modal visibility in tests, trigger modal disable when feature gets activated

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -625,6 +625,7 @@ Cypress.Commands.add( 'saveFeatureSettings', () => {
  * Enable Feature.
  */
 Cypress.Commands.add( 'enableFeature', () => {
+	cy.disableCredentialReuseModal();
 	cy.get( '.classifai-enable-feature-toggle input[type="checkbox"]' ).check();
 } );
 
@@ -851,4 +852,24 @@ Cypress.Commands.add( 'classicCreateProduct', ( { title, content } ) => {
 	cy.visit( '/wp-admin/post-new.php?post_type=product' );
 	cy.get( '#title' ).type( title );
 	cy.get( '#content' ).type( content );
+} );
+
+/**
+ * Disable credential reuse modal for all tests.
+ * This prevents the modal from appearing when enabling features.
+ */
+Cypress.Commands.add( 'disableCredentialReuseModal', () => {
+	cy.window().then( ( win ) => {
+		win.localStorage.setItem( 'classifai_dont_ask_credential_reuse', 'true' );
+	} );
+} );
+
+/**
+ * Enable credential reuse modal (for testing the modal itself).
+ * This allows the modal to appear when enabling features.
+ */
+Cypress.Commands.add( 'enableCredentialReuseModal', () => {
+	cy.window().then( ( win ) => {
+		win.localStorage.removeItem( 'classifai_dont_ask_credential_reuse' );
+	} );
 } );
