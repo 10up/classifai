db339d1177bcfd966bd60fc0be2dd3612f21871f
Darin Kotter <darin.kotter@gmail.com>
2025-06-27 10:31:03 -0600

    Change to wp_strip_all_tags

--- includes/Classifai/Helpers.php
+++ includes/Classifai/Helpers.php
@@ -415,7 +415,7 @@ function get_all_post_statuses(): array {
 	);
 
 	// Remove any HTML from the statuses.
-	$all_statuses = array_map( 'strip_tags', $all_statuses );
+	$all_statuses = array_map( 'wp_strip_all_tags', $all_statuses );
 
 	/*
 	 * There is a minor difference in the label for 'pending' status between
