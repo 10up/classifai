fcc5c4790f4bb07bfac1f4faa7a4309b9177dda5
Luiz Miguel Axcar <luiz.axcar@10up.com>
2025-08-19 22:01:21 -0300

    Add LocalHost Provider APIRequest

---
+++ includes/Classifai/Providers/Localhost/APIRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Classifai\Providers\Localhost;
+
+use Classifai\Providers\HTTPClient;
+use WP_Error;
+
+/**
+ * The APIRequest class is a low level class to make Localhost API
+ * requests.
+ *
+ * The returned response is parsed into JSON and returned as an
+ * associative array.
+ *
+ * Usage:
+ *
+ * $request = new Classifai\Providers\Localhost\APIRequest();
+ * $request->post( $localhost_url, $options );
+ */
+class APIRequest extends HTTPClient {
+
+	/**
+	 * Localhost APIRequest constructor.
+	 *
+	 * @param string $api_key API key (not used for localhost, kept for compatibility).
+	 * @param string $feature Feature name.
+	 */
+	public function __construct( string $api_key = '', string $feature = '' ) {
+		parent::__construct( $api_key, $feature );
+	}
+
+	/**
+	 * Get the filter prefix for this provider.
+	 *
+	 * @return string
+	 */
+	protected function get_filter_prefix(): string {
+		return 'classifai_localhost';
+	}
+
+	/**
+	 * Add authentication header.
+	 *
+	 * @param array $options The header options, passed by reference.
+	 */
+	protected function add_auth_header( array &$options ) {
+		// Not needed for localhost providers.
+	}
+
+	/**
+	 * Get the auth header.
+	 *
+	 * @return string
+	 */
+	public function get_auth_header() {
+		// Not needed for localhost providers.
+		return '';
+	}
+
+	/**
+	 * Get the Localhost API key.
+	 *
+	 * @return string
+	 */
+	public function get_api_key():string {
+		return $this->api_key;
+	}
+}
