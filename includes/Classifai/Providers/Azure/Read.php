0b4655c60c7f11ced4475bea7c7bcb65e9a53452
Peter Sorensen <peter.sorensen@fueled.com>
2025-10-08 20:51:14 -0700

    restore missing function

--- includes/Classifai/Providers/Azure/Read.php
+++ includes/Classifai/Providers/Azure/Read.php
@@ -11,6 +11,7 @@ namespace Classifai\Providers\Azure;
 use WP_Error;
 use function Classifai\computer_vision_max_filesize;
 use function Classifai\safe_wp_remote_get;
+use function Classifai\safe_wp_remote_post;
 
 /**
  * Read class
