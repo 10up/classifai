8d1b501648fcb2d095936e044ab6a52e74fa29b1
Darin Kotter <darin.kotter@gmail.com>
2026-07-15 09:52:43 -0600

    Version bump on dev

--- classifai.php
+++ classifai.php
@@ -4,7 +4,7 @@
  * Plugin URI:        https://github.com/10up/classifai
  * Update URI:        https://classifaiplugin.com
  * Description:       Enhance your WordPress content with Artificial Intelligence and Machine Learning services.
- * Version:           3.9.0
+ * Version:           4.0.0-dev
  * Requires at least: 6.9
  * Requires PHP:      7.4
  * Author:            10up
